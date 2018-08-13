e898359422030b4ae7667fa0babcf532c3d119df
Michał Sypko <m.sypko@livechatinc.com>
2018-08-13 18:08:30 +0200

    All toasts

--- src/components/Toast/index.d.ts
+++ src/components/Toast/index.d.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export type ToastHorizontalPosition = 'left' | 'center' | 'right';
 export type ToastVerticalPosition = 'top' | 'middle' | 'bottom';
 
@@ -9,9 +10,23 @@ export interface ToastProps {
     horizontalPosition?: ToastHorizontalPosition;
     verticalPosition?: ToastVerticalPosition;
     fixed?: boolean;
+=======
+import * as React from 'react';
+
+export interface ButtonProps {
+    children?: string;
+    id?: string;
+>>>>>>> All toasts
     success?: boolean;
     warning?: boolean;
     error?: boolean;
     info?: boolean;
+<<<<<<< HEAD
     onClose?(): void;
+=======
+    onClick?(): void;
+    onFocus?(): void;
+    onBlur?(): void;
+    renderIcon?: string | React.ReactNode;
+>>>>>>> All toasts
 }
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Tooltip, TooltipContent } from './Tooltip';
 export { default as Button } from './Button';
 export { default as Toast } from './Toast';
 export { default as Normalize } from './Normalize';
+export { default as Toast } from './Toast';
