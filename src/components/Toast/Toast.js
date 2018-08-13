4aca2630a477a2c16f7768fadc388877a3e9e7b8
Michał Sypko <m.sypko@livechatinc.com>
2018-08-13 18:15:10 +0200

    indentation fixes

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -7,89 +7,87 @@ import styles from './style.css';
 const cx = classNames.bind(styles);
 
 const Toast = props => {
-    const {
-        children,
-        success,
-        warning,
-        error,
-        info,
-        className,
-        ...toastProps
-    } = props;
+  const {
+    children,
+    success,
+    warning,
+    error,
+    info,
+    className,
+    ...toastProps
+  } = props;
 
-    let toastType = 'notification';
+  let toastType = 'notification';
 
-    if (success) {
-        toastType = 'success';
-    } else if (warning) {
-        toastType = 'warning';
-    } else if (error) {
-        toastType = 'error';
-    } else if (info) {
-        toastType = 'info';
-    }
+  if (success) {
+    toastType = 'success';
+  } else if (warning) {
+    toastType = 'warning';
+  } else if (error) {
+    toastType = 'error';
+  } else if (info) {
+    toastType = 'info';
+  }
 
-    const componentClassNames = `
-    ${cx({
-        toast: true,
-        'toast--success': success,
-        'toast--warning': warning,
-        'toast--error': error,
-        'toast--info': info,
-    })} ${className}
-  `;
+  const componentClassNames = `
+  ${cx({
+    toast: true,
+    'toast--success': success,
+    'toast--warning': warning,
+    'toast--error': error,
+    'toast--info': info,
+  })} ${className}
+`;
 
-    return (
-        <div
-            {...toastProps}
-            className={componentClassNames}
-        >
-            <div className={cx({
-                'toast-icon': true,
-            })}>
-                <InformationIcon />
-            </div>
-            <div className={cx({
-                'toast-content': true,
-            })}>
-                {children}
-            </div>
-            <div className={cx({
-                'toast-close': true,
-            })}>
-                <CloseIcon />
-            </div>
-        </div>
-    );
+  return (
+    <div
+      {...toastProps}
+      className={componentClassNames}
+    >
+      <div className={cx({
+        'toast-icon': true,
+      })}>
+        <InformationIcon />
+      </div>
+      <div className={cx({
+        'toast-content': true,
+      })}>
+        {children}
+      </div>
+      <div className={cx({'toast-close': true,})}>
+        <CloseIcon />
+      </div>
+    </div>
+  );
 };
 
 Toast.propTypes = {
-    children: PropTypes.node.isRequired,
-    id: PropTypes.string,
-    /**
-     * Type of toast
-     */
-    success: PropTypes.bool,
-    /**
-     * Type of toast
-     */
-    warning: PropTypes.bool,
-    /**
-     * Type of toast
-     */
-    error: PropTypes.bool,
-    /**
-     * Type of toast
-     */
-    info: PropTypes.bool,
-    onClick: PropTypes.func,
-    onFocus: PropTypes.func,
-    onBlur: PropTypes.func,
-    className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  id: PropTypes.string,
+  /**
+   * Type of toast
+   */
+  success: PropTypes.bool,
+  /**
+   * Type of toast
+   */
+  warning: PropTypes.bool,
+  /**
+   * Type of toast
+   */
+  error: PropTypes.bool,
+  /**
+   * Type of toast
+   */
+  info: PropTypes.bool,
+  onClick: PropTypes.func,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+  className: PropTypes.string,
 };
 
 Toast.defaultProps = {
-    className: ''
+  className: ''
 };
 
 export default Toast;
