adfb614330e98c7feae94960d912e5dcda520994
Michał Sypko <m.sypko@livechatinc.com>
2018-08-14 18:47:30 +0200

    correct path

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,4 @@ export { default as Tab, TabsWrapper, TabsList } from './Tab';
 export { default as Tooltip, TooltipContent } from './Tooltip';
 export { default as Button } from './Button';
 export { default as Toast } from './Toast';
-export { default as Normalize } from './Normalize/Normalize';
+export { default as Normalize } from './Normalize';
