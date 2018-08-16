95cd0751b8e8d2a2eeb19cd1be0cdc2643c7bf1a
Michał Sypko <m.sypko@livechatinc.com>
2018-08-16 14:26:30 +0200

    fully remove Normalize from Toast branch

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,3 @@ export { default as Tab, TabsWrapper, TabsList } from './Tab';
 export { default as Tooltip, TooltipContent } from './Tooltip';
 export { default as Button } from './Button';
 export { default as Toast } from './Toast';
-export { default as Normalize } from './Normalize';
-export { default as Toast } from './Toast';
