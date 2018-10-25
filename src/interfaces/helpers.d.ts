966e3e73d8592a5b5ca3bcdc2d380825cc8a68d9
kamilmateusiak <k.mateusiak@livechatinc.com>
2018-10-25 23:06:44 +0200

    removing wrong import in helpers types

--- src/interfaces/helpers.d.ts
+++ src/interfaces/helpers.d.ts
@@ -1,5 +1,3 @@
-import { string } from "prop-types";
-
 export var helpers: {
   generateNotificationUniqueId: (notifications: {[id: string]: { [key: string]: any; }}) => string;
   getMergedClassNames: (classNames: string, classNameProperty: string) => string;
