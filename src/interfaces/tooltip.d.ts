119f57397d739bb3fa2defa29f0ab31418255740
Dobrochna Przybyłek <d.przybylek@livechatinc.com>
2018-11-05 16:19:29 +0100

    toolitipcontent interface

--- src/interfaces/tooltip.d.ts
+++ src/interfaces/tooltip.d.ts
@@ -2,14 +2,26 @@ export type TooltipAlign = 'top' | 'center' | 'bottom' | 'left' | 'right;'
 
 export interface ITooltipProps {
   className?: string;
-  keepContentVisibleOnHover?: boolean,
-  isTooltipVisible?: boolean,
-  offset?: 0,
-  trigger?: string,
-  align?: TooltipAlign,
-  directions?: string[],
-  children: React.ReactNode,
-  content: React.ReactNode
+  keepContentVisibleOnHover?: boolean;
+  isTooltipVisible?: boolean;
+  offset?: number;
+  trigger?: string;
+  align?: TooltipAlign;
+  directions?: string[];
+  children: React.ReactNode;
+  content: React.ReactNode;
 }
 
 export var Tooltip: React.ComponentType<ITooltipProps>;
+
+export interface ITooltipContentProps {
+  children: React.ReactNode;
+  align?: TooltipAlign;
+  arrowOffset?:	number;
+  arrowPosition?: TooltipAlign;
+  backgroundColor?: string;
+  className?: string;
+  fontColor?: string;
+}
+
+export var TooltipContent: React.ComponentType<ITooltipContentProps>;
