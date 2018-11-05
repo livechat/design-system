7e26c0dda489b18d7c492f14bb4e2754b0460a2c
dprzybylek <dobrochna.przybylek@gmail.com>
2018-11-05 23:10:10 +0100

    feature/tooltip-interface
    * tooltip and tooltip content ts typings

---
+++ src/interfaces/tooltip.d.ts
@@ -0,0 +1,30 @@
+export type TooltipAlign = 'top' | 'center' | 'bottom' | 'left' | 'right';
+export type TooltipDirection = 'top' | 'bottom' | 'left' | 'right';
+export type TooltipArrowPosition = 'top' | 'bottom' | 'left' | 'right';
+export type TooltipTrigger = 'hover' | 'click' |'custom';
+
+export interface ITooltipProps {
+  className?: string;
+  keepContentVisibleOnHover?: boolean;
+  isTooltipVisible?: boolean;
+  offset?: number;
+  trigger?: TooltipTrigger;
+  align?: TooltipAlign;
+  directions?: TooltipDirection[];
+  children: React.ReactNode;
+  content: React.ReactNode;
+}
+
+export var Tooltip: React.ComponentType<ITooltipProps>;
+
+export interface ITooltipContentProps {
+  children: React.ReactNode;
+  align?: TooltipAlign;
+  arrowOffset?:	number;
+  arrowPosition?: TooltipArrowPosition;
+  backgroundColor?: string;
+  className?: string;
+  fontColor?: string;
+}
+
+export var TooltipContent: React.ComponentType<ITooltipContentProps>;
