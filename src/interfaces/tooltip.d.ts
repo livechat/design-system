f92cd097f5fb481654ae8bf7d0fb2cede3324f6a
Dobrochna Przybyłek <d.przybylek@livechatinc.com>
2018-11-05 16:46:46 +0100

    cr fix

--- src/interfaces/tooltip.d.ts
+++ src/interfaces/tooltip.d.ts
@@ -1,13 +1,16 @@
-export type TooltipAlign = 'top' | 'center' | 'bottom' | 'left' | 'right;'
+export type TooltipAlign = 'top' | 'center' | 'bottom' | 'left' | 'right';
+export type TooltipDirection = 'top' | 'bottom' | 'left' | 'right';
+export type TooltipArrowPosition = 'top' | 'bottom' | 'left' | 'right';
+export type TooltipTrigger = 'hover' | 'click' |'custom';
 
 export interface ITooltipProps {
   className?: string;
   keepContentVisibleOnHover?: boolean;
   isTooltipVisible?: boolean;
   offset?: number;
-  trigger?: string;
+  trigger?: TooltipTrigger;
   align?: TooltipAlign;
-  directions?: string[];
+  directions?: TooltipDirection[];
   children: React.ReactNode;
   content: React.ReactNode;
 }
@@ -18,7 +21,7 @@ export interface ITooltipContentProps {
   children: React.ReactNode;
   align?: TooltipAlign;
   arrowOffset?:	number;
-  arrowPosition?: TooltipAlign;
+  arrowPosition?: TooltipArrowPosition;
   backgroundColor?: string;
   className?: string;
   fontColor?: string;
