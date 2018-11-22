49d7e5f17d6a97b1d46a3476a07a23b25c133965
kamilmateusiak <k.mateusiak@livechatinc.com>
2018-11-22 15:24:51 +0100

    build improvements

--- src/components/MultiSelectField/MultiSelectField.js
+++ src/components/MultiSelectField/MultiSelectField.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
 import MultiSelect from './MultiSelect';
 import TextField from '../TextField';
 
