7391cde2dc6ab14d6fb38a21bd360b553610684c
Michał Sypko <m.sypko@livechatinc.com>
2019-01-25 17:28:01 +0100

    DS-3 - ts interfaces update

--- src/interfaces/inapps.d.ts
+++ src/interfaces/inapps.d.ts
@@ -5,13 +5,12 @@ import { IButtonProps } from './buttons';
 export interface IInAppBaseProps extends React.HTMLAttributes<HTMLDivElement> {
     className?: string;
     header?: {
-        avatarSrc?: IInAppHeaderProps['avatarSrc'],
-        avatarAlt?: IInAppHeaderProps['avatarAlt'],
+        avatar?: IInAppHeaderProps['avatar'],
         text?: IInAppHeaderProps['text']
     };
     onClose(): void;
     closeOnEscPress?: boolean;
-    footer?: React.ReactNode;
+    footerButtons?: IInAppFooterProps['buttons'];
 }
 
 export interface IInAppPortalProps {
@@ -20,15 +19,12 @@ export interface IInAppPortalProps {
 }
 
 export interface IInAppProps extends IInAppBaseProps {
-    imageSrc?: string;
-    imageAlt?: string;
-    heading?: React.ReactNode;
+    image?: IInAppImage;
 }
 
 export interface IInAppHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
     className?: string;
-    avatarSrc?: string;
-    avatarAlt?: string;
+    avatar?: IInAppAvatar;
     text?: React.ReactNode;
     onCloseButtonClick: void;
 }
@@ -41,9 +37,15 @@ export interface IInAppFooterProps extends React.HTMLAttributes<HTMLDivElement> 
     };
 }
 
+export interface IInAppAvatar extends React.HTMLAttributes<HTMLImageElement> {}
+
+export interface IInAppImage extends React.HTMLAttributes<HTMLImageElement> {}
+
 export var InAppBase: React.ComponentType<IInAppBaseProps>;
 export var InAppPortal: React.ComponentType<IInAppPortalProps>;
 export var InApp: React.ComponentType<IInAppProps>;
 export var InAppHeader: React.ComponentType<IInAppHeaderProps>;
 export var InAppFooter: React.ComponentType<IInAppFooterProps>;
 export var InAppBody: React.ComponentType<React.HTMLAttributes<HTMLDivElement>>;
+export var InAppAvatar: React.ComponentType<IInAppAvatar>;
+export var InAppImage: React.ComponentType<IInAppImage>;
