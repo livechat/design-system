f9f60b89b7e70ef2936e66f6786773861f232de9
Dobrochna Przybyłek <d.przybylek@livechatinc.com>
2019-05-29 15:26:57 +0200

    handle classnames

--- src/components/Divider/Divider.js
+++ src/components/Divider/Divider.js
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import styles from './style.scss';
 
-const Divider = props => <div className={styles.divider} {...props} />;
+import getMergedClassNames from '../../utils/getMergedClassNames';
+
+const baseClass = 'divider';
+
+const Divider = props => {
+  const { className, ...restProps } = props;
+  const mergedClassNames = getMergedClassNames(styles[baseClass], className);
+
+  return <div className={mergedClassNames} {...restProps} />;
+};
 
 export default Divider;
