25527f22c300a6935da65a360c9d5bf74a9c3695
kamilmateusiak <k.mateusiak@livechatinc.com>
2019-06-06 13:55:25 +0200

    Changing props of Modal component (#76)
    - footer property is now optional

--- packages/design-system/src/interfaces/modals.d.ts
+++ packages/design-system/src/interfaces/modals.d.ts
@@ -2,19 +2,19 @@
 
 export interface IModalBaseProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
-  closeOnEscPress?: boolean; 
+  closeOnEscPress?: boolean;
   onClose(): void;
 }
 
 export interface IModalProps extends IModalBaseProps {
-  heading?: React.ReactNode,
-  footer: React.ReactNode,
+  heading?: React.ReactNode;
+  footer?: React.ReactNode;
 }
 
 export interface IActionModalProps extends IModalBaseProps {
-  heading?: React.ReactNode,
-  icon?: React.ReactNode,
-  actions?: React.ReactNode
+  heading?: React.ReactNode;
+  icon?: React.ReactNode;
+  actions?: React.ReactNode;
 }
 
 export interface IModalPortalProps {
@@ -26,10 +26,22 @@ export var ModalBase: React.ComponentType<IModalBaseProps>;
 export var ModalPortal: React.ComponentType<IModalPortalProps>;
 export var ActionModal: React.ComponentType<IActionModalProps>;
 export var Modal: React.ComponentType<IModalProps>;
-export var ModalHeader: React.ComponentType<React.HTMLAttributes<HTMLDivElement>>;
-export var ModalFooter: React.ComponentType<React.HTMLAttributes<HTMLDivElement>>;
+export var ModalHeader: React.ComponentType<
+  React.HTMLAttributes<HTMLDivElement>
+>;
+export var ModalFooter: React.ComponentType<
+  React.HTMLAttributes<HTMLDivElement>
+>;
 export var ModalBody: React.ComponentType<React.HTMLAttributes<HTMLDivElement>>;
-export var ActionModalActions: React.ComponentType<React.HTMLAttributes<HTMLDivElement>>;
-export var ActionModalContent: React.ComponentType<React.HTMLAttributes<HTMLDivElement>>;
-export var ActionModalIcon: React.ComponentType<React.HTMLAttributes<HTMLDivElement>>;
-export var ActionModalHeading: React.ComponentType<React.HTMLAttributes<HTMLHeadingElement>>;
+export var ActionModalActions: React.ComponentType<
+  React.HTMLAttributes<HTMLDivElement>
+>;
+export var ActionModalContent: React.ComponentType<
+  React.HTMLAttributes<HTMLDivElement>
+>;
+export var ActionModalIcon: React.ComponentType<
+  React.HTMLAttributes<HTMLDivElement>
+>;
+export var ActionModalHeading: React.ComponentType<
+  React.HTMLAttributes<HTMLHeadingElement>
+>;
