3694407ac2d98c584cc5e5569a7063b6bf42e1fa
Maciej Walaszczyk <walaszczykm@gmail.com>
2019-06-24 08:43:25 +0200

    button group tests

---
+++ src/components/ButtonGroup/ButtonGroup.test.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import ButtonGroup from './ButtonGroup';
+import Button from '../Button';
+
+const onChangeFn = jest.fn();
+
+describe('ButtonGroup', () => {
+  it('should render empty button group', () => {
+    const component = shallow(<ButtonGroup />);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render button group with button', () => {
+    const component = shallow(
+      <ButtonGroup>
+        <Button />
+      </ButtonGroup>
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should handle current index change', () => {
+    const component = shallow(
+      <ButtonGroup onChange={onChangeFn}>
+        <Button id="button1" />
+        <Button id="button2" />
+        <Button id="button3" />
+      </ButtonGroup>
+    );
+
+    component.find('#button1').simulate('click');
+    expect(onChangeFn).toBeCalledWith(0);
+
+    component.find('#button2').simulate('click');
+    expect(onChangeFn).toBeCalledWith(1);
+
+    component.find('#button3').simulate('click');
+    expect(onChangeFn).toBeCalledWith(2);
+  });
+
+  it('should use current index from porps', () => {
+    const component = shallow(
+      <ButtonGroup currentIndex={1}>
+        <Button />
+      </ButtonGroup>
+    );
+
+    expect(component.state('currentIndex')).toBe(1);
+  });
+});
