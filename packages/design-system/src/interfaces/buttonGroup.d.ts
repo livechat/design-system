96bf486c7f648c9abf5ae55ed056d4093f43dfbc
Maciej Walaszczyk <walaszczykm@gmail.com>
2019-07-16 13:05:50 +0200

    ButtonGroup component (#87)
    * ButtonGroup component
    
    * use internal state for tracking active elements
    
    * docs and types
    
    * remove not needed style from button
    
    * move default index value to constant
    
    * use derived state
    
    * button group tests
    
    * fix spelling in docs
    
    * fix problem with overlaping buttons outline
    
    * update test snapshot
    
    * add className prop to IButtonGroupProps
    
    * handle onClick from child
    
    * pass event in onIndexChange callback

---
+++ packages/design-system/src/interfaces/buttonGroup.d.ts
@@ -0,0 +1,17 @@
+// <reference types="react" />
+import { ButtonSize } from "./buttons";
+
+export interface IButtonGroupProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  fullWidth?: boolean;
+  className?: string;
+  currentIndex?: number;
+  size: ButtonSize;
+  children: React.ReactNode;
+  onIndexChange?: (
+    index: number,
+    event: React.MouseEvent<HTMLDivElement>
+  ) => void;
+}
+
+export var ButtonGroup: React.ComponentType<IButtonGroupProps>;
