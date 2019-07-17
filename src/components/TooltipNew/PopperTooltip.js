ea7e2b82af7f4095ce974743dbf88691994b81d7
Norbo <norbo@MacBook-Pro-Norbo.local>
2019-07-17 19:48:09 +0200

    Added themes to the tooltips

--- src/components/TooltipNew/PopperTooltip.js
+++ src/components/TooltipNew/PopperTooltip.js
@@ -233,6 +233,7 @@ class PopperTooltip extends React.PureComponent {
     const {
       children,
       className,
+      theme,
       closeOnOutsideClick,
       hoverOutDelayTimeout,
       zIndex,
@@ -342,6 +343,7 @@ class PopperTooltip extends React.PureComponent {
 PopperTooltip.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  theme: PropTypes.oneOf(['normal', 'invert', 'important']),
   /**
    * Use `closeOnOutsideClick=true` when you want tooltip to be closed on click outside it.
    * If you are using `triggerActionType='managed'` event handler will call provided onClose prop.
@@ -421,6 +423,7 @@ PopperTooltip.propTypes = {
 };
 
 PopperTooltip.defaultProps = {
+  theme: 'normal',
   hoverOutDelayTimeout: 100,
   modifiers: {},
   onClose: noop,
