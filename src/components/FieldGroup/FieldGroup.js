06c97e0123e83a211cfa1c2fac307cb1b48cf632
Dobrochna <d.przybylek@livechatinc.com>
2019-08-05 11:57:05 +0200

    handle error in field group

--- src/components/FieldGroup/FieldGroup.js
+++ src/components/FieldGroup/FieldGroup.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import FieldError from '../FieldError';
 import styles from './style.scss';
 import getMergedClassNames from '../../utils/getMergedClassNames';
 
 const cx = classNames.bind(styles);
 
 const FieldGroup = props => {
-  const { className, children, inline, stretch, ...restProps } = props;
+  const { className, children, inline, stretch, error, ...restProps } = props;
 
   const baseClass = 'field-group';
   const mergedClassNames = getMergedClassNames(
@@ -22,15 +23,17 @@ const FieldGroup = props => {
   return (
     <div {...restProps} className={mergedClassNames}>
       {children}
+      {error && <FieldError>{error}</FieldError>}
     </div>
   );
 };
 
 FieldGroup.propTypes = {
+  children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  error: PropTypes.string,
   inline: PropTypes.bool,
-  stretch: PropTypes.bool,
-  children: PropTypes.node.isRequired
+  stretch: PropTypes.bool
 };
 
 export default FieldGroup;
