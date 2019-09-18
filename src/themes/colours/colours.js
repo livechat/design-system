8402aec6a033ea57d5cde7d1501ee7f7ae98731d
Dobrochna <d.przybylek@livechatinc.com>
2019-09-18 13:16:22 +0200

    colours poc

---
+++ src/themes/colours/colours.js
@@ -0,0 +1,7 @@
+export const grey = 'rgba(255, 255, 255, 0);';
+export const grey10 = 'rgba(255, 255, 255, 0.1);';
+export const grey70 = 'rgba(255, 255, 255, 0.7);';
+export const grey100 = grey;
+
+export const white = 'rgba(255, 255, 255, 1)';
+export const white100 = white;
---
+++ src/themes/themes/default.js
@@ -0,0 +1,7 @@
+import { grey, grey70, white, grey10 } from '../colours/colours';
+
+export const text01 = grey;
+export const text03 = grey70;
+export const text04 = grey10;
+
+export const background01 = white;
