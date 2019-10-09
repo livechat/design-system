72b7de90cd79358337f386254e5ecfe2cc641460
kamilmateusiak <k.mateusiak@livechatinc.com>
2019-10-09 15:21:33 +0200

    Adding omit type to support typescript in version 3.1 (#138)

--- src/interfaces/switch.d.ts
+++ src/interfaces/switch.d.ts
@@ -1,5 +1,6 @@
 export type SwitchSize = "basic" | "compact";
 
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 type HTMLInputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, "size">;
 
 export interface ISwitchProps extends HTMLInputProps {
