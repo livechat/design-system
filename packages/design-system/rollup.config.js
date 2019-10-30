a0846f476546f0c86d9ad4d4ca171aad93c8c08a
Michał Sypko <m.sypko@livechatinc.com>
2019-10-30 13:37:45 +0100

    keep dist in core package

--- packages/design-system/rollup.config.js
+++ packages/design-system/rollup.config.js
@@ -92,7 +92,7 @@ const umdConfig = mergeAll([
   configBase,
   {
     output: {
-      file: `../../dist/core/${pkg.name.replace(/@.*\//, '')}.js`,
+      file: `dist/${pkg.name.replace(/@.*\//, '')}.js`,
       format: 'umd',
       name: 'DesignSystem',
       globals: {
