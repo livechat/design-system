a522b54f77946b816bcf1c759ad289744b518054
dprzybylek <d.przybylek@livechatinc.com>
2020-06-18 10:14:27 +0200

    Banners promo (#190)

---
+++ packages/design-system/src/interfaces/promo.d.ts
@@ -0,0 +1,16 @@
+import { MouseEventHandler } from "react";
+
+export type PromoSize = "small" | "medium" | "large";
+
+export interface IProgressBarProps extends React.HTMLAttributes<HTMLDivElement> {
+    buttonText?: string;
+    children: React.ReactNode;
+    header: string;
+    img?: string;
+    light?:boolean;
+    linkText?: string;
+    onButtonClick?: MouseEventHandler<HTMLButtonElement>;
+    onClose?: MouseEventHandler<HTMLButtonElement>;
+    onLinkClick?: MouseEventHandler<HTMLButtonElement>;
+    size?: PromoSize;
+}
