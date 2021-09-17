6582294dac8a3424d4a5ff6d299bdd60d4de1720
Marcin Sawicki <m.sawicki@livechatinc.com>
2021-09-17 12:38:06 +0200

    missing changes

--- packages/react-components/src/stories/Promo.stories.tsx
+++ packages/react-components/src/stories/Promo.stories.tsx
@@ -1,33 +1,38 @@
-import React from "react";
-import { ComponentMeta } from "@storybook/react";
+import * as React from 'react';
+import { ComponentMeta } from '@storybook/react';
 
-import { Promo as PromoComponent, PromoSize } from "../components/Promo";
+import {
+  IPromoProps,
+  Promo as PromoComponent,
+  PromoSize,
+} from '../components/Promo';
 
 export default {
-  title: "Components/Promo",
+  title: 'Components/Promo',
   component: PromoComponent,
 } as ComponentMeta<typeof PromoComponent>;
 
-export const Promo = (args) => (
+type IPromoArgs = IPromoProps;
+
+export const Promo = (args: IPromoArgs): React.ReactElement => (
   <div>
     <PromoComponent {...args}>
       <>
-        A description with a <b>maximum of 100 characters</b>. 
-        That usually means only one or two sentences.
+        A description with a <b>maximum of 100 characters</b>. That usually
+        means only one or two sentences.
       </>
     </PromoComponent>
   </div>
 );
 
-<Promo size="small" header="This example headline has 40 characters" onClose={()=>window.alert('close')} linkText="Link button" buttonText="Primary action"   img="https://via.placeholder.com/100"
-> A description with a <b>maximum of 100</b> characters. That usually means only one or two sentences. </Promo>
-
 Promo.args = {
   buttonText: null,
-  header: "This example headline has 40 characters",
-  img: "https://via.placeholder.com/100",
+  header: 'This example headline has 40 characters',
+  img: 'https://via.placeholder.com/100',
   light: false,
   linkText: false,
   size: PromoSize.Small,
-  onClose: () => { alert('onClose click') }
-}
+  onClose: () => {
+    alert('onClose click');
+  },
+};
