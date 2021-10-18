7f97ad8a0fda0f4764fb90fd94398ab1133bbc2a
przemuh <przemuh@gmail.com>
2021-10-18 13:17:52 +0200

    Added sizes

--- packages/react-components/src/stories/Button.stories.tsx
+++ packages/react-components/src/stories/Button.stories.tsx
@@ -8,12 +8,26 @@ export default {
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-export const allKinds = () => (
+export const kinds = () => (
   <div className="spacer">
     <Button>Basic</Button>
     <Button kind="primary">Primary</Button>
     <Button kind="secondary">Secondary</Button>
     <Button kind="destructive">Destructive</Button>
     <Button kind="text">Text</Button>
+  </div>
+);
+
+export const sizes = () => (
+  <div className="spacer">
+    <Button size="compact" kind="primary">
+      Compact
+    </Button>
+    <Button size="medium" kind="primary">
+      Medium (default)
+    </Button>
+    <Button size="large" kind="primary">
+      Large
+    </Button>
   </div>
 );
