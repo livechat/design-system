fd45b448e21bd814cb856e0b5f2914497611c463
przemuh <przemuh@gmail.com>
2021-10-18 14:27:04 +0200

    Fix import path in Badge.spec

--- packages/react-components/src/components/Badge/Badge.spec.tsx
+++ packages/react-components/src/components/Badge/Badge.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from '../test-utils';
+import { render } from '../../test-utils';
 import { Badge } from './Badge';
 
 describe('<Badge> component', () => {
