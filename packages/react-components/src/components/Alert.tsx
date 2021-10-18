a684cb8283c4cec569930e8ee1286ca68aa26390
Marcin Sawicki <sawicki.marcin.88@gmail.com>
2021-10-18 15:44:10 +0200

    DS-152 - Banner component (#262)
    * Banner component
    
    * promo component
    
    * resolve missing packages, changes related to review comments
    
    * merge with master branch + refactor
    
    * missing changes
    
    * toast component + story + some changes
    
    * toast wrapper
    
    * new stylelint rule
    
    * Promo stories + unit tests
    
    * updated Promo stories and unit tests
    
    * Banner stories + unit tests
    
    * Toast stories + unit tests
    
    * ToastWrapper unit tests and some improvements
    
    * changes after review
    
    * update package-locks
    
    * changes in designs after review
    
    * change lint params for fixing styles
    
    * components rename
    
    * changes after review
    
    * stories with templates

---
+++ packages/react-components/src/components/Alert.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import cx from 'classnames';
+// TODO: remove and use the Icon wrapper with correct icon after migration
+import CloseIcon from 'react-material-icon-svg/dist/CloseIcon';
+
+import { AlertIcon } from './AlertIcon';
+
+const baseClass = 'lc-alert';
+
+export enum AlertSize {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+}
+
+export enum AlertType {
+  Info = 'info',
+  Warning = 'warning',
+  Success = 'success',
+  Error = 'error',
+}
+
+export interface IAlertProps {
+  className?: string;
+  size?: AlertSize;
+  type?: AlertType;
+  onClose?: () => void;
+}
+
+export const Alert: React.FC<IAlertProps> = ({
+  children,
+  className,
+  size = AlertSize.Small,
+  type = AlertType.Info,
+  onClose,
+}) => {
+  const mergedClassNames = cx(
+    baseClass,
+    `${baseClass}--${type}`,
+    `${baseClass}--${size}`,
+    className
+  );
+
+  return (
+    <div className={mergedClassNames}>
+      <div className={`${baseClass}__content`}>
+        <AlertIcon type={type} />
+        <div className={`${baseClass}__content-text`}>{children}</div>
+      </div>
+      {onClose && (
+        <button
+          type="button"
+          className={`${baseClass}__close-icon`}
+          onClick={onClose}
+        >
+          {/* TODO: remove and use the Icon wrapper with correct icon after migration */}
+          <CloseIcon fill="#424d57" />
+        </button>
+      )}
+    </div>
+  );
+};
---
+++ packages/react-components/src/components/PromoBanner.tsx
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import cx from 'classnames';
+// TODO: remove and use the Icon wrapper with correct icon after migration
+import CloseIcon from 'react-material-icon-svg/dist/CloseIcon';
+
+// TODO: Remove after `Button` component migration
+interface IButtonProps {
+  children: string;
+  className?: string;
+  onClick?(): void;
+}
+const Button = ({ children, className, onClick }: IButtonProps) => (
+  <button className={className} onClick={onClick}>
+    {children}
+  </button>
+);
+
+const baseClass = 'lc-promo-banner';
+
+export enum PromoBannerSize {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+}
+
+export interface IPromoBannerProps {
+  className?: string;
+  buttonText?: string;
+  header: string;
+  img?: string;
+  light?: boolean;
+  linkText?: string;
+  size?: PromoBannerSize;
+  onButtonClick?: () => void;
+  onClose?: () => void;
+  onLinkClick?: () => void;
+}
+
+export const PromoBanner: React.FC<IPromoBannerProps> = ({
+  className,
+  buttonText,
+  children,
+  header,
+  img,
+  light,
+  linkText,
+  size = PromoBannerSize.Small,
+  onButtonClick,
+  onClose,
+  onLinkClick,
+}) => {
+  const mergedClassNames = cx(
+    baseClass,
+    {
+      [`${baseClass}--light`]: light,
+      [`${baseClass}--${size}`]: size,
+    },
+    className
+  );
+
+  const shouldRenderLargeFooter = (buttonText || linkText) && size === 'large';
+  const shouldRenderSmallOrMediumFooter =
+    (buttonText || linkText) && size !== 'large';
+
+  const footer = (
+    <div className={`${baseClass}__footer`}>
+      {buttonText && (
+        // TODO: Use `Button` component after migration
+        //
+        // <Button
+        //   kind="primary"
+        //   size="compact"
+        //   onClick={onButtonClick}
+        //   className={styles[`${baseClass}__button-text`]}
+        // >
+        //   {buttonText}
+        // </Button>
+
+        <Button onClick={onButtonClick} className={`${baseClass}__button-text`}>
+          {buttonText}
+        </Button>
+      )}
+      {linkText && (
+        // TODO: Use `Button` component after migration
+        //
+        // <Button size="compact" kind="text" onClick={onLinkClick}>
+        //   {linkText}
+        // </Button>
+
+        <Button onClick={onLinkClick}>{linkText}</Button>
+      )}
+    </div>
+  );
+
+  return (
+    <div className={mergedClassNames}>
+      <div className={`${baseClass}__content`}>
+        {img && <img src={img} className={`${baseClass}__img`} />}
+        <div className={`${baseClass}__wrapper`}>
+          <div className={`${baseClass}__header`}>{header}</div>
+          <div>{children}</div>
+          {shouldRenderSmallOrMediumFooter && footer}
+        </div>
+        {shouldRenderLargeFooter && footer}
+      </div>
+      {onClose && (
+        <button
+          type="button"
+          className={`${baseClass}__close-icon`}
+          onClick={onClose}
+        >
+          {/* TODO: remove and use the Icon wrapper with correct icon after migration */}
+          <CloseIcon fill="#424d57" />
+        </button>
+      )}
+    </div>
+  );
+};
