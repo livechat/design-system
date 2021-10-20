92481889a78645b0bd2b32f63ddd7d1eb30ea802
VadymBezpalko <12149393+VadymBezpalko@users.noreply.github.com>
2021-10-20 20:42:40 +0200

    DS-170: RadioButton (#272)
    * DS-170: RadioButton WIP
    
    * DS-170: RadioButton adding missed focus and hover modes
    
    * DS-170: RadioButton styling improvements
    
    * DS-170: RadioButton storybook fix
    
    * DS-170: RadioButton adding tests and installing @testing-library/user-event
    
    * DS-170: RadioButton minor improvement
    
    * DS-170: Radio review improvements
    
    * DS-170: implementing typography tokens and fixing margins
    
    * DS-170: fixing styles
    
    * DS-170: review fixes
    
    * DS-170: switching to typography token components
    
    * DS-170: storybook improvement

---
+++ packages/react-components/src/components/RadioButton.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import cx from 'classnames';
+import { FieldDescription } from './FieldDescription';
+import { Text } from './Text';
+
+export interface IRadioButtonProps
+  extends React.HTMLAttributes<HTMLInputElement> {
+  description?: React.ReactNode;
+  checked?: boolean;
+  disabled?: boolean;
+}
+
+const baseClass = 'lc-radio-button';
+
+export const RadioButton: React.FC<IRadioButtonProps> = ({
+  children,
+  className = '',
+  description,
+  checked,
+  disabled,
+  ...props
+}) => {
+  const mergedClassNames = cx(baseClass, {
+    [className]: !!className,
+    [`${baseClass}--selected`]: checked,
+    [`${baseClass}--disabled`]: disabled,
+  });
+
+  return (
+    <div className={mergedClassNames}>
+      <label className={`${baseClass}__label`}>
+        <div className={`${baseClass}__circle`}>
+          <span className={`${baseClass}__inner-circle`} />
+          <input
+            className={`${baseClass}__input`}
+            {...props}
+            type="radio"
+            checked={checked}
+            disabled={disabled}
+          />
+        </div>
+        <Text as="div" size="md" className={`${baseClass}__text`}>
+          {children}
+        </Text>
+      </label>
+      {description && (
+        <FieldDescription className={`${baseClass}__helper`}>
+          {description}
+        </FieldDescription>
+      )}
+    </div>
+  );
+};
