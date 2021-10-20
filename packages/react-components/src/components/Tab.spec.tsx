5de3c67bade17fc247de7c1b58cc508f7179a6ad
Maciej Walaszczyk <walaszczykm@gmail.com>
2021-10-20 15:20:38 +0200

    Tab component

---
+++ packages/react-components/src/components/Tab.spec.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { render } from '../test-utils';
+import { Tab } from './Tab';
+
+describe('<Tab /> component', () => {
+  it('should render unselected Tab by default', () => {
+    const { getByText } = render(<Tab>Hello</Tab>);
+    expect(getByText('Hello')).not.toHaveClass('lc-tab--selected');
+  });
+
+  it("should render selected Tab if 'isSelected' is provided", () => {
+    const { getByText } = render(<Tab isSelected>Hello</Tab>);
+    expect(getByText('Hello')).toHaveClass('lc-tab--selected');
+  });
+
+  it('should render properly formatted description', () => {
+    const { getByText } = render(<Tab description="1">Hello</Tab>);
+    expect(getByText('(1)')).toBeInTheDocument();
+  });
+
+  it('should render with button element by default', () => {
+    const { getByRole } = render(<Tab>Hello</Tab>);
+    expect(getByRole('button')).toHaveTextContent('Hello');
+  });
+
+  it("should render with anchor element if 'href' is provided", () => {
+    const { getByRole } = render(<Tab href="http://example.com">Hello</Tab>);
+    const link = getByRole('link');
+    expect(link).toHaveTextContent('Hello');
+    expect(link).toHaveAttribute('href', 'http://example.com');
+  });
+});
--- packages/react-components/src/index.ts
+++ packages/react-components/src/index.ts
@@ -3,3 +3,4 @@ import '../../styles/dist/styles.css';
 export { Badge } from './components/Badge';
 export { Icon } from './components/Icon';
 export { Card } from './components/Card';
+export { Tab } from './components/Tab';
