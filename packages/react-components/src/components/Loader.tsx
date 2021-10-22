a3521de9e113ff27b1c907ab7cd49a73888aa12c
Przemek <p.suchodolski@livechat.com>
2021-10-22 11:40:34 +0200

    DS-156 Buttons (#271)
    ADDED:
    - Button component
    - Loader component
    
    BREAKING CHANGES:
    - Loader doesn't take custom classNames for inner elements
    - Button doesn't shadow the aria- props via custom props anymore
    
    NEW FEATURES:
    - Icon only button
    
    FIXED:
    - Inconsistent visual state for disabled & loading states. Now both states are displayed as disabled.

---
+++ packages/react-components/src/components/Loader.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import cx from 'classnames';
+
+import { Text } from './Text';
+
+const baseClass = 'lc-loader';
+const spinnerClass = `${baseClass}__spinner`;
+
+export interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  size?: 'small' | 'medium' | 'large';
+  label?: string;
+  /** Fragment circle color */
+  primaryColor?: string;
+  /** Main circle color */
+  secondaryColor?: string;
+}
+
+export const Loader: React.FC<Props> = ({
+  primaryColor,
+  secondaryColor,
+  label,
+  className,
+  size = 'medium',
+}) => {
+  return (
+    <div className={cx(baseClass, className)}>
+      <div className={cx(spinnerClass, `${spinnerClass}--${size}`)}>
+        <div
+          className="lc-loader__spinner-circle"
+          style={{
+            /* stylelint-disable */
+            borderColor: secondaryColor,
+            borderTopColor: primaryColor,
+          }}
+        />
+      </div>
+      {label && (
+        <Text as="div" size="md" className="lc-loader__label">
+          {label}
+        </Text>
+      )}
+    </div>
+  );
+};
