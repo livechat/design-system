36e77be3f14be2d0a9c9cae0f3f97b20e361cb94
przemuh <przemuh@gmail.com>
2021-11-12 10:34:34 +0100

    Added theme switcher

--- packages/react-components/.storybook/preview.js
+++ packages/react-components/.storybook/preview.js
@@ -3,6 +3,7 @@ import '@livechat/design-system-styles';
 import './global.css';
 
 export const parameters = {
+  backgrounds: { disable: true },
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     exclude: ['className'],
@@ -10,5 +11,13 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  themes: {
+    default: 'legacy',
+    list: [
+      { name: 'legacy', class: 'lc-legacy-theme', color: '#4284f5' },
+      { name: 'light', class: 'lc-light-theme', color: '#06f' },
+      { name: 'dark', class: 'lc-dark-theme', color: '#000' },
+    ],
   },
 };
