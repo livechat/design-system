7ba74e3cf8791364b95c9446f087b87e229a36f1
Przemek <p.suchodolski@livechat.com>
2021-11-23 10:35:52 +0100

    Theme switcher for storybook (#287)
    * Added theme switcher
    * Set default content color

--- packages/react-components/.storybook/main.js
+++ packages/react-components/.storybook/main.js
@@ -1,8 +1,13 @@
-const foundations = ['Typography', 'DesignTokens', 'Icons'].map(
+const foundations = ['Typography', 'ColorTokens', 'Icons'].map(
   (name) => `../src/stories/foundations/${name}.stories.mdx`
 );
 
 module.exports = {
   stories: [...foundations, '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-addon-themes',
+  ],
+
 };
