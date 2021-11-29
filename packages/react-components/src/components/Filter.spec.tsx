13367f3bcc75fe59cc85a88ebf2c01906ecb340e
Vadym Bezpalko <v.bezpalko@livechat.com>
2021-11-29 17:25:01 +0100

    DS-00-grouping-input: test update

--- packages/react-components/src/components/Filter.spec.tsx
+++ packages/react-components/src/components/Filter.spec.tsx
@@ -1,45 +1,38 @@
 import * as React from 'react';
+import { render } from '../test-utils';
+import { Filter } from './Filter';
 import userEvent from '@testing-library/user-event';
-import { render } from '../../test-utils';
-import { Tag } from './Tag';
 
 jest.mock('@livechat/design-system-icons/dist/material', () => ({
   Close: () => <div />,
 }));
 
-const baseClass = 'lc-tag-input__tag';
-const update = jest.fn();
-const remove = jest.fn();
-const inputRef: React.RefObject<HTMLInputElement> = {
-  current: null,
-};
+const baseClass = 'lc-filter';
+const onRemove = jest.fn();
 const tagIndex = 0;
 const props = {
-  update,
-  remove,
+  onRemove,
   index: tagIndex,
-  inputRef,
 };
 
-describe('<Tag> component', () => {
+describe('<Filter> component', () => {
   it('should have error class when error occurs', () => {
     const { container } = render(
-      <Tag {...props} validator={() => false}>
+      <Filter {...props} error>
         tag1
-      </Tag>
+      </Filter>
     );
     expect(container.firstChild).toHaveClass(`${baseClass}--error`);
   });
 
   it('should call remove method on remove button press', () => {
     const { getByRole } = render(
-      <Tag {...props} validator={() => false}>
+      <Filter {...props} dismissible>
         tag1
-      </Tag>
+      </Filter>
     );
     const removeButton = getByRole('button');
     userEvent.click(removeButton);
-    expect(remove).toBeCalledTimes(1);
-    expect(remove).toBeCalledWith(tagIndex);
+    expect(onRemove).toBeCalledTimes(1);
   });
 });
