bea384e459762fd1a2d4b4f5bd2ab6d82bfc31a7
Vadym Bezpalko <v.bezpalko@livechat.com>
2021-12-20 15:40:17 +0100

    AA-12778: adding custom color prop

--- packages/react-components/src/components/Tag.tsx
+++ packages/react-components/src/components/Tag.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import cx from 'classnames';
-import { Text } from './Text';
+import { getContrast } from 'polished';
 import { Close } from '@livechat/design-system-icons/dist/material';
+import { Text } from './Text';
 import Icon, { IconSizeName } from './Icon';
 
 const baseClass = 'lc-tag';
@@ -9,12 +10,22 @@ const baseClass = 'lc-tag';
 export interface ITagProps extends React.HTMLAttributes<HTMLDivElement> {
   kind?: 'default' | 'info' | 'warning' | 'success' | 'error';
   size?: 'medium' | 'large';
+  customColor?: string;
   dismissible?: boolean;
   outline?: boolean;
   onRemove?(): void;
   icon?: React.FC<React.SVGProps<SVGSVGElement>> | string;
   avatar?: string;
 }
+
+const getCustomTextClass = (customColor?: string) => {
+  if (!customColor) {
+    return '';
+  }
+  return getContrast(customColor, '#FFFFFF') > 4.5
+    ? 'text-white'
+    : 'text-black';
+};
 
 export const Tag: React.FC<ITagProps> = ({
   className = '',
@@ -26,6 +37,7 @@ export const Tag: React.FC<ITagProps> = ({
   outline = false,
   icon,
   avatar,
+  customColor,
   ...restProps
 }) => {
   const mergedClassNames = cx(
@@ -37,11 +49,34 @@ export const Tag: React.FC<ITagProps> = ({
       [`${baseClass}--dismissible`]: dismissible,
       [`${baseClass}--outline`]: outline,
       [`${baseClass}--with-icon`]: !!icon || !!avatar,
+      [`${baseClass}--${getCustomTextClass(customColor)}`]: !!customColor,
     }
   );
 
+  const getCustomColorStyles = () => {
+    if (!customColor) {
+      return {};
+    }
+    if (outline) {
+      return {
+        style: {
+          'background-color': 'transparent',
+          color: customColor,
+          'border-color': customColor,
+        },
+      };
+    }
+    return { style: { 'background-color': customColor } };
+  };
+
   return (
-    <Text className={mergedClassNames} {...restProps} as="div" size="md">
+    <Text
+      className={mergedClassNames}
+      {...restProps}
+      {...getCustomColorStyles()}
+      as="div"
+      size="md"
+    >
       {avatar && (
         <img className={`${baseClass}__avatar`} src={avatar} alt="tag-avatar" />
       )}{' '}
