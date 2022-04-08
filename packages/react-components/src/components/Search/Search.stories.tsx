2c6a9ebc533cbd12156aa57a540949db8fdd5e0a
Marcin Sawicki <m.sawicki@livechatinc.com>
2022-04-08 09:01:50 +0200

    fix after review

--- packages/react-components/src/components/Search/Search.stories.tsx
+++ packages/react-components/src/components/Search/Search.stories.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import { ComponentMeta, Story } from '@storybook/react';
 
-import { Search as SearchComonent, ISearchProps, SearchSize } from './Search';
+import { Search as SearchComponent, ISearchProps, SearchSize } from './Search';
 
 export default {
   title: 'Components/Search',
-  component: SearchComonent,
-} as ComponentMeta<typeof SearchComonent>;
+  component: SearchComponent,
+} as ComponentMeta<typeof SearchComponent>;
 
 const containerStyles = { width: 300, marginBottom: 15 };
 const textStyles = { marginBottom: 5, fontSize: 15 };
 
 const StoryTemplate: Story<ISearchProps> = (args: ISearchProps) => (
   <div style={{ width: 300 }}>
-    <SearchComonent {...args} />
+    <SearchComponent {...args} />
   </div>
 );
 
@@ -26,19 +26,19 @@ export const states = (args: ISearchProps): React.ReactElement => (
   <div>
     <div style={containerStyles}>
       <div style={textStyles}>Basic</div>
-      <SearchComonent {...args} />
+      <SearchComponent {...args} />
     </div>
     <div style={containerStyles}>
       <div style={textStyles}>Disabled</div>
-      <SearchComonent {...args} isDisabled />
+      <SearchComponent {...args} isDisabled />
     </div>
     <div style={containerStyles}>
       <div style={textStyles}>Loading</div>
-      <SearchComonent {...args} isLoading />
+      <SearchComponent {...args} isLoading />
     </div>
     <div style={containerStyles}>
       <div style={textStyles}>Collapsable</div>
-      <SearchComonent {...args} isCollapsable />
+      <SearchComponent {...args} isCollapsable />
     </div>
   </div>
 );
@@ -51,15 +51,15 @@ export const sizes = (args: ISearchProps): React.ReactElement => (
   <div>
     <div style={containerStyles}>
       <div style={textStyles}>Compact</div>
-      <SearchComonent {...args} size={SearchSize.Compact} />
+      <SearchComponent {...args} size={SearchSize.Compact} />
     </div>
     <div style={containerStyles}>
       <div style={textStyles}>Medium</div>
-      <SearchComonent {...args} size={SearchSize.Medium} />
+      <SearchComponent {...args} size={SearchSize.Medium} />
     </div>
     <div style={containerStyles}>
       <div style={textStyles}>Large</div>
-      <SearchComonent {...args} size={SearchSize.Large} />
+      <SearchComponent {...args} size={SearchSize.Large} />
     </div>
   </div>
 );
