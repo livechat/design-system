b08ae85ae584d543ed5276a057609a2000e58c28
MichalPaszowski <m.paszowski@livechat.com>
2022-04-08 13:39:56 +0200

    test fix

--- packages/react-components/src/components/Tooltip/Tooltip.spec.tsx
+++ packages/react-components/src/components/Tooltip/Tooltip.spec.tsx
@@ -1,12 +1,35 @@
 import * as React from 'react';
-import { render, fireEvent } from 'test-utils';
+import { render, fireEvent, cleanup } from 'test-utils';
+import { vi } from 'vitest';
+
 import { Tooltip } from './Tooltip';
 
-global.ResizeObserver = jest.fn().mockImplementation(() => ({
-  observe: jest.fn(),
-  unobserve: jest.fn(),
-  disconnect: jest.fn(),
-}));
+vi.mock('@floating-ui/react-dom', () => {
+  return {
+    useFloating: vi.fn(() => {
+      return {
+        x: 0,
+        y: 0,
+        placement: 'bottom',
+        middlewareData: {
+          arrow: {
+            x: 0,
+            y: 0,
+          },
+        },
+        refs: {
+          floating: 0,
+          reference: 0,
+        },
+      };
+    }),
+    arrow: vi.fn(),
+    autoUpdate: vi.fn(),
+    flip: vi.fn(),
+    offset: vi.fn(),
+    x: 0,
+  };
+});
 
 describe('<Tooltip> component', () => {
   it('should show content on mouse over', () => {
@@ -23,5 +46,7 @@ describe('<Tooltip> component', () => {
     );
 
     expect(container).toHaveTextContent('Test');
+
+    cleanup();
   });
 });
