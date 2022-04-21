0d6204e441f713bae0401470edfc81fcfb7c7700
przemuh <przemuh@gmail.com>
2022-04-21 22:59:05 +0200

    Radio button

--- packages/react-components/src/components/RadioButton/RadioButton.tsx
+++ packages/react-components/src/components/RadioButton/RadioButton.tsx
@@ -7,48 +7,49 @@ import styles from './RadioButton.module.scss';
 
 export interface RadioButtonProps
   extends React.HTMLAttributes<HTMLInputElement> {
-  description?: React.ReactNode;
+  description?: string;
   checked?: boolean;
   disabled?: boolean;
 }
 
 const baseClass = 'radio-button';
 
-export const RadioButton: React.FC<RadioButtonProps> = ({
-  children,
-  className = '',
-  description,
-  checked,
-  disabled,
-  ...props
-}) => {
-  const mergedClassNames = cx(styles[baseClass], className, {
-    [styles[`${baseClass}--selected`]]: checked,
-    [styles[`${baseClass}--disabled`]]: disabled,
-  });
+export const RadioButton = React.forwardRef<HTMLInputElement, RadioButtonProps>(
+  (
+    { children, className = '', description, checked, disabled, ...props },
+    ref
+  ) => {
+    const mergedClassNames = cx(styles[baseClass], className, {
+      [styles[`${baseClass}--selected`]]: checked,
+      [styles[`${baseClass}--disabled`]]: disabled,
+    });
 
-  return (
-    <div className={mergedClassNames}>
-      <label className={styles[`${baseClass}__label`]}>
-        <div className={styles[`${baseClass}__circle`]}>
-          <span className={styles[`${baseClass}__inner-circle`]} />
-          <input
-            className={styles[`${baseClass}__input`]}
-            {...props}
-            type="radio"
-            checked={checked}
-            disabled={disabled}
-          />
-        </div>
-        <Text as="div" size="md" className={styles[`${baseClass}__text`]}>
-          {children}
-        </Text>
-      </label>
-      {description && (
-        <FieldDescription className={styles[`${baseClass}__helper`]}>
-          {description}
-        </FieldDescription>
-      )}
-    </div>
-  );
-};
+    return (
+      <div className={mergedClassNames}>
+        <label className={styles[`${baseClass}__label`]}>
+          <div className={styles[`${baseClass}__circle`]}>
+            <span className={styles[`${baseClass}__inner-circle`]} />
+            <input
+              {...props}
+              className={styles[`${baseClass}__input`]}
+              ref={ref}
+              type="radio"
+              checked={checked}
+              disabled={disabled}
+            />
+          </div>
+          {children && (
+            <Text as="div" size="md" className={styles[`${baseClass}__text`]}>
+              {children}
+            </Text>
+          )}
+        </label>
+        {description && (
+          <FieldDescription className={styles[`${baseClass}__helper`]}>
+            {description}
+          </FieldDescription>
+        )}
+      </div>
+    );
+  }
+);
