d6d577f56737e671cdbee85d104ca3dfd1bb1762
Szymon Graczyk <s.graczyk@livechat.com>
2022-07-25 14:19:05 +0200

    #341 Reorganize Badge component to reflect Figma designs

--- packages/react-components/src/components/Badge/Badge.stories.tsx
+++ packages/react-components/src/components/Badge/Badge.stories.tsx
@@ -1,24 +1,41 @@
+import { Story } from '@storybook/react';
 import * as React from 'react';
-import { Badge as BadgeComponent, BadgeProps } from './Badge';
+import { Badge, BadgeProps } from './Badge';
 
 export default {
   title: 'Components/Badge',
-  component: BadgeComponent,
+  component: Badge,
 };
 
-export const Badge = (args: BadgeProps): React.ReactElement => (
-  <div
-    style={{
-      display: 'inline-flex',
-      alignItems: 'center',
-      alignContent: 'center',
-    }}
-  >
-    <BadgeComponent {...args} />
+export const Default: Story<BadgeProps> = (
+  args: BadgeProps
+): React.ReactElement => <Badge {...args} />;
+
+Default.storyName = 'Badge';
+Default.args = {
+  children: '1',
+};
+
+export const Sizes = (): JSX.Element => (
+  <div className="spacer">
+    <Badge size="compact">1</Badge>
+    <Badge size="medium">1</Badge>
+    <Badge size="large">1</Badge>
   </div>
 );
 
-Badge.args = {
-  secondary: false,
-  children: '7',
-};
+export const Kinds = (): JSX.Element => (
+  <div className="spacer">
+    <Badge kind="primary">1</Badge>
+    <Badge kind="secondary">1</Badge>
+    <Badge kind="tertiary">1</Badge>
+  </div>
+);
+
+export const Types = (): JSX.Element => (
+  <div className="spacer">
+    <Badge type="content">3 steps left</Badge>
+    <Badge type="alert" />
+    <Badge type="dot" />
+  </div>
+);
