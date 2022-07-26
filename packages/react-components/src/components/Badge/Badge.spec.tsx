0eed2c7d52a861ed06ebc5bbe5c1744a110bdd4d
Szymon Graczyk <s.graczyk@livechat.com>
2022-07-26 15:43:21 +0200

    #341 Reorganize Badge component to reflect Figma designs (#356)
    * Added `kind` prop instead of `secondary` boolean
    * Added `size` to handle 3 cases
    * Added `type` to automatically handle `alert` and `dot` variants

--- packages/react-components/src/components/Badge/Badge.spec.tsx
+++ packages/react-components/src/components/Badge/Badge.spec.tsx
@@ -2,11 +2,31 @@ import * as React from 'react';
 import { render } from 'test-utils';
 
 import { Badge } from './Badge';
+import styles from './Badge.module.scss';
 
 describe('Badge', () => {
   it('should accept custom className', () => {
     const { container } = render(<Badge className="my-custom-class" />);
 
     expect(container.firstChild).toHaveClass('my-custom-class');
+  });
+
+  it('should display content passed as children by default', () => {
+    const content = '3 steps left';
+    const { container } = render(<Badge>{content}</Badge>);
+
+    expect(container).toHaveTextContent(content);
+  });
+
+  it('should display exclamation mark for alert type', () => {
+    const { container } = render(<Badge type="alert" />);
+
+    expect(container).toHaveTextContent('!');
+  });
+
+  it('should display dot content for dot type', () => {
+    const { container } = render(<Badge type="dot" />);
+
+    expect(container.querySelector(`.${styles['badge__dot']}`)).toBeVisible();
   });
 });
--- packages/react-components/src/components/Badge/Badge.stories.tsx
+++ packages/react-components/src/components/Badge/Badge.stories.tsx
@@ -1,24 +1,62 @@
 import * as React from 'react';
-import { Badge as BadgeComponent, BadgeProps } from './Badge';
+import { Story } from '@storybook/react';
+
+import { StoryDescriptor } from '../../stories/components/StoryDescriptor';
+
+import { Badge, BadgeProps } from './Badge';
 
 export default {
   title: 'Components/Badge',
-  component: BadgeComponent,
+  component: Badge,
 };
 
-export const Badge = (args: BadgeProps): React.ReactElement => (
-  <div
-    style={{
-      display: 'inline-flex',
-      alignItems: 'center',
-      alignContent: 'center',
-    }}
-  >
-    <BadgeComponent {...args} />
-  </div>
+export const Default: Story<BadgeProps> = (
+  args: BadgeProps
+): React.ReactElement => <Badge {...args} />;
+
+Default.storyName = 'Badge';
+Default.args = {
+  children: '1',
+};
+
+export const Sizes = (): JSX.Element => (
+  <>
+    <StoryDescriptor title="Compact">
+      <Badge size="compact">1</Badge>
+    </StoryDescriptor>
+    <StoryDescriptor title="Medium">
+      <Badge size="medium">1</Badge>
+    </StoryDescriptor>
+    <StoryDescriptor title="Large">
+      <Badge size="large">1</Badge>
+    </StoryDescriptor>
+  </>
 );
 
-Badge.args = {
-  secondary: false,
-  children: '7',
-};
+export const Kinds = (): JSX.Element => (
+  <>
+    <StoryDescriptor title="Primary">
+      <Badge kind="primary">1</Badge>
+    </StoryDescriptor>
+    <StoryDescriptor title="Secondary">
+      <Badge kind="secondary">1</Badge>
+    </StoryDescriptor>
+    <StoryDescriptor title="Tertiary">
+      <Badge kind="tertiary">1</Badge>
+    </StoryDescriptor>
+  </>
+);
+
+export const Types = (): JSX.Element => (
+  <>
+    <StoryDescriptor title="Content">
+      <Badge type="content">3 steps left</Badge>
+    </StoryDescriptor>
+    <StoryDescriptor title="Alert">
+      <Badge type="alert" />
+    </StoryDescriptor>
+    <StoryDescriptor title="Dot">
+      <Badge type="dot" />
+    </StoryDescriptor>
+  </>
+);
