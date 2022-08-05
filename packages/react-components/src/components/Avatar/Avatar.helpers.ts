39bd267f7daf73a87c0ec8779f6cedf33cfa22cd
Szymon Graczyk <s.graczyk@livechat.com>
2022-08-05 10:09:06 +0200

    #373 Avatar component (#375)

---
+++ packages/react-components/src/components/Avatar/Avatar.helpers.ts
@@ -0,0 +1,16 @@
+import { getContrast } from 'polished';
+
+export function getInitials(name = '', count = 2): string {
+  return name
+    .split(' ')
+    .map((el) => el.charAt(0))
+    .join('')
+    .substring(0, count)
+    .toUpperCase();
+}
+
+export function getFontColor(color: string): string {
+  return getContrast(color, '#FFFFFF') > 4.5
+    ? 'var(--color-white)'
+    : 'var(--color-black)';
+}
