6d00b8b208d76c4e3b2f2763f897d19f692b3f9c
Szymon Graczyk <s.graczyk@livechat.com>
2022-08-26 15:32:47 +0200

    #390 Card component refactoring

--- packages/react-components/src/components/Card/Card.spec.tsx
+++ packages/react-components/src/components/Card/Card.spec.tsx
@@ -1,29 +1,38 @@
 import * as React from 'react';
-import { render } from 'test-utils';
+import { render, fireEvent, vi } from 'test-utils';
 import { Card } from './Card';
 
-describe('<Card /> component', () => {
-  it('should render only title if image is not provided', () => {
+describe('<Card> component', () => {
+  it('should allow for custom class', () => {
+    const { container } = render(
+      <Card title="Hello" className="my-css-class">
+        test
+      </Card>
+    );
+    expect(container.firstChild).toHaveClass('my-css-class');
+  });
+
+  it('should display only title if image is not provided', () => {
     const { getByText, getByRole } = render(<Card title="Hello" />);
     expect(getByText('Hello')).toBeVisible();
     expect(() => getByRole('img')).toThrow();
   });
 
-  it('should render title and image if provided', () => {
+  it('should display title and image if provided', () => {
     const { getByText, getByRole } = render(
-      <Card title="Hello" img="https://via.placeholder.com/100" />
+      <Card title="Hello" src="https://via.placeholder.com/100" />
     );
     expect(getByText('Hello')).toBeVisible();
     expect(getByRole('img')).toBeVisible();
   });
 
-  it('should render text children', () => {
+  it('should display text children', () => {
     const { getByText } = render(<Card title="Hello">Lorem ipsum</Card>);
     expect(getByText('Lorem ipsum')).toBeVisible();
   });
 
-  it('should render nested elements as children', () => {
-    const { getByRole } = render(
+  it('should display nested elements as children', () => {
+    const { getAllByRole } = render(
       <Card title="Hello">
         <div>
           <h3>Lorem ipsum</h3>
@@ -31,8 +40,70 @@ describe('<Card /> component', () => {
       </Card>
     );
 
-    const heading = getByRole('heading');
-    expect(heading).toBeVisible();
-    expect(heading).toHaveTextContent('Lorem ipsum');
+    const [, nestedHeading] = getAllByRole('heading');
+    expect(nestedHeading).toBeVisible();
+    expect(nestedHeading).toHaveTextContent('Lorem ipsum');
+  });
+
+  it('should display nested elements as children', () => {
+    const { getAllByRole } = render(
+      <Card title="Hello">
+        <div>
+          <h3>Lorem ipsum</h3>
+        </div>
+      </Card>
+    );
+
+    const [, nestedHeading] = getAllByRole('heading');
+    expect(nestedHeading).toBeVisible();
+    expect(nestedHeading).toHaveTextContent('Lorem ipsum');
+  });
+
+  it('should allow expanding hidden content', () => {
+    const expandableText = 'dolor sit amet';
+    const { getByText } = render(
+      <Card title="Hello" expandableContent={expandableText}>
+        Lorem ipsum
+      </Card>
+    );
+
+    const expander = getByText('Show more');
+    expect(() => getByText(expandableText)).toThrow();
+    fireEvent.click(expander);
+    expect(getByText(expandableText)).toBeVisible();
+    fireEvent.click(expander);
+    expect(() => getByText(expandableText)).toThrow();
+  });
+
+  it('should display action buttons', () => {
+    const secondaryAction = vi.fn();
+    const destructiveAction = vi.fn();
+    const { getByText } = render(
+      <Card
+        title="Hello"
+        buttonsOptions={[
+          {
+            kind: 'secondary',
+            onClick: secondaryAction,
+            children: 'Details',
+          },
+          {
+            kind: 'destructive',
+            onClick: destructiveAction,
+            children: 'Delete',
+          },
+        ]}
+      >
+        Lorem ipsum
+      </Card>
+    );
+
+    const secondaryButton = getByText('Details');
+    fireEvent.click(secondaryButton);
+    expect(secondaryAction).toHaveBeenCalled();
+
+    const destructiveButton = getByText('Delete');
+    fireEvent.click(destructiveButton);
+    expect(destructiveAction).toHaveBeenCalled();
   });
 });
--- packages/react-components/src/components/Card/Card.tsx
+++ packages/react-components/src/components/Card/Card.tsx
@@ -1,34 +1,111 @@
 import * as React from 'react';
 import cx from 'clsx';
+import {
+  ChevronUp,
+  ChevronDown,
+} from '@livechat/design-system-icons/react/material';
+
 import { Text, Heading } from '../Typography';
+import { Button, ButtonProps } from '../Button';
+import { Icon } from '../Icon';
+
 import styles from './Card.module.scss';
 
+export type CardActionType = 'none' | 'buttons' | 'expand';
+export type CardButtonOptions = Pick<
+  ButtonProps,
+  'children' | 'kind' | 'onClick'
+>;
+
 export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  alt?: string;
+  buttonsOptions?: CardButtonOptions[];
+  description?: string;
+  expandableContent?: React.ReactNode;
+  src?: string;
   title: string;
-  img?: string;
 }
 
+const baseClass = 'card';
+const headerClass = `${baseClass}__header`;
+const headingClass = `${headerClass}__heading`;
+const actionsClass = `${baseClass}__actions`;
+
 export const Card: React.FC<CardProps> = ({
-  img,
-  title,
+  alt,
+  buttonsOptions = [],
   children,
   className,
-  ...restProps
+  description,
+  expandableContent,
+  src,
+  title,
+  ...divProps
 }) => {
+  const [isExpanded, setIsExpanded] = React.useState(false);
+  const expandIcon = isExpanded ? ChevronUp : ChevronDown;
+  const expandButtonText = isExpanded ? 'Hide' : 'Show more';
+  const shouldShowActionButtons = buttonsOptions?.length > 0;
+  const shouldShowExpandAction = !!expandableContent;
+  const shouldShowActions = shouldShowActionButtons || shouldShowExpandAction;
+
   return (
-    <Text
-      as="div"
-      size="md"
-      className={cx(styles['card'], className)}
-      {...restProps}
-    >
-      {(img || title) && (
-        <Heading as="div" size="sm" className={styles['card__title']}>
-          {img && <img src={img} className={styles['card__img']} />}
-          {title && <div className={styles['card__text']}>{title}</div>}
-        </Heading>
+    <div className={cx(styles[baseClass], className)} {...divProps}>
+      <div className={styles[headerClass]}>
+        {src && (
+          <img
+            alt={alt}
+            className={styles[`${headerClass}__image`]}
+            src={src}
+          />
+        )}
+        <div className={styles[headingClass]}>
+          <Heading size="sm" className={styles[`${headingClass}__title`]}>
+            {title}
+          </Heading>
+          <Text size="sm" className={styles[`${headingClass}__description`]}>
+            {description}
+          </Text>
+        </div>
+      </div>
+      <Text as="div" size="md" className={styles[`${baseClass}__content`]}>
+        {children}
+      </Text>
+      {isExpanded && (
+        <Text
+          as="div"
+          size="md"
+          className={styles[`${baseClass}__expanded-content`]}
+        >
+          {expandableContent}
+        </Text>
       )}
-      {children}
-    </Text>
+      {shouldShowActions && (
+        <div className={styles[actionsClass]}>
+          <div className={styles[`${actionsClass}__line`]} />
+          <div className={styles[`${actionsClass}__buttons`]}>
+            {shouldShowActionButtons &&
+              buttonsOptions.map(
+                ({ kind, onClick, children: buttonChildren }) => (
+                  <Button size="compact" kind={kind} onClick={onClick}>
+                    {buttonChildren}
+                  </Button>
+                )
+              )}
+            {shouldShowExpandAction && (
+              <Button
+                className={styles[`${actionsClass}__buttons-expander`]}
+                kind="plain"
+                iconPosition="right"
+                icon={<Icon source={expandIcon} />}
+                onClick={() => setIsExpanded(!isExpanded)}
+              >
+                {expandButtonText}
+              </Button>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
