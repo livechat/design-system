7461ba61f4c1fcd139a3f4a9ffba455c73082ac0
Michal Paszowski <michal.paszowski@gmail.com>
2022-09-09 14:25:18 +0200

    #380 introduce black-locked token and use it with switch icon (#402)
    * introduce black-locked token and use it with switch icon
    
    * apply properly color to the icon
    
    * apply style
    
    * proper content black locked colours

--- packages/react-components/src/components/Switch/Switch.tsx
+++ packages/react-components/src/components/Switch/Switch.tsx
@@ -110,6 +110,7 @@ export const Switch: React.FC<SwitchProps> = ({
           )}
           {isLocked && (
             <Icon
+              className={styles[`${baseClass}__icon`]}
               data-testid="lock-icon"
               size={iconSize}
               source={LockIcon}
