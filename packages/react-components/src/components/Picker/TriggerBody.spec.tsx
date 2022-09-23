4902477af4f8dc2ec1a1911297bb46e0aa62c760
Marcin Sawicki <m.sawicki@livechat.com>
2022-09-23 13:50:28 +0200

    more tests for Picker components

--- packages/react-components/src/components/Picker/TriggerBody.spec.tsx
+++ packages/react-components/src/components/Picker/TriggerBody.spec.tsx
@@ -19,7 +19,7 @@ const renderComponent = (props: ITriggerBodyProps) => {
 };
 
 describe('<TriggerBody> component', () => {
-  it('should display placeholder if there is no selected items', () => {
+  it('should display placeholder if there is no selected items and not isOpen', () => {
     const { getByText } = renderComponent(defaultProps);
 
     expect(getByText('Select option')).toBeVisible();
@@ -39,9 +39,67 @@ describe('<TriggerBody> component', () => {
     const { getByRole } = renderComponent({
       ...defaultProps,
       isOpen: true,
+      onFilter,
     });
 
     userEvent.type(getByRole('textbox'), 'option');
     expect(onFilter).toBeCalledWith('option');
+  });
+
+  it('should show only one item in single mode', () => {
+    const { queryByText } = renderComponent({
+      ...defaultProps,
+      items: [
+        { key: 'one', name: 'Option one' },
+        { key: 'two', name: 'Option two' },
+        { key: 'three', name: 'Option three' },
+      ],
+    });
+
+    expect(queryByText('Option one')).toBeVisible();
+    expect(queryByText('Option two')).toBeNull();
+    expect(queryByText('Option three')).toBeNull();
+  });
+
+  it('should show all items in multiselect mode', () => {
+    const { queryByText } = renderComponent({
+      ...defaultProps,
+      type: 'multi',
+      items: [
+        { key: 'one', name: 'Option one' },
+        { key: 'two', name: 'Option two' },
+        { key: 'three', name: 'Option three' },
+      ],
+    });
+
+    expect(queryByText('Option one')).toBeVisible();
+    expect(queryByText('Option two')).toBeVisible();
+    expect(queryByText('Option three')).toBeVisible();
+  });
+
+  it('should call onItemRemove with item selected to remove', () => {
+    const onItemRemove = vi.fn();
+    const { queryAllByRole } = renderComponent({
+      ...defaultProps,
+      type: 'multi',
+      items: [
+        { key: 'one', name: 'Option one' },
+        { key: 'two', name: 'Option two' },
+        { key: 'three', name: 'Option three' },
+      ],
+      onItemRemove,
+    });
+    const removeButtons = queryAllByRole('button');
+
+    userEvent.click(removeButtons[0]);
+    expect(onItemRemove).toHaveBeenCalledWith({
+      key: 'one',
+      name: 'Option one',
+    });
+    userEvent.click(removeButtons[2]);
+    expect(onItemRemove).toHaveBeenCalledWith({
+      key: 'three',
+      name: 'Option three',
+    });
   });
 });
