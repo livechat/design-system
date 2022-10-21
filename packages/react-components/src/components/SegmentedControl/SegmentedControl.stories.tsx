98184c827797f6412430daad3155e146a24ff563
Michal Paszowski <michal.paszowski@gmail.com>
2022-10-21 10:50:32 +0200

    #396 ButtonGroup rename SegmentedControl + component improvements (#422)
    * segmented control
    
    * lading status fixes
    
    * simpler
    
    * component rebuilt
    
    * taking state from button props
    
    * picking props and choosing array for state
    
    * story remade to fit new convention
    
    * away with state props
    
    * selective size options

---
+++ packages/react-components/src/components/SegmentedControl/SegmentedControl.stories.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { ComponentMeta, Story } from '@storybook/react';
+
+import { SegmentedControl, SegmentedControlProps } from './SegmentedControl';
+
+const buttonSizes = ['compact', 'medium', 'large'];
+
+export default {
+  title: 'Components/Segmented Control',
+  component: SegmentedControl,
+  argTypes: {
+    size: {
+      options: buttonSizes,
+      control: {
+        type: 'select',
+        labels: buttonSizes,
+      },
+    },
+  },
+} as ComponentMeta<typeof SegmentedControl>;
+
+export const Default: Story<SegmentedControlProps> = (
+  args: SegmentedControlProps
+) => <SegmentedControl {...args} />;
+
+Default.storyName = 'Controlled';
+Default.args = {
+  buttons: [
+    { id: 'one', label: 'one', loading: true, disabled: true },
+    { id: 'two', label: 'two', disabled: true },
+    { id: 'three', label: 'three' },
+    { id: 'fourth', label: 'fourth' },
+  ],
+  initialId: 'fourth',
+};
+
+export const Uncontrolled: Story<SegmentedControlProps> = (
+  args: SegmentedControlProps
+) => <SegmentedControl {...args} />;
+
+Uncontrolled.storyName = 'Uncontrolled With Initial Selection';
+Uncontrolled.args = {
+  buttons: [
+    { id: 'one', label: 'one' },
+    { id: 'two', label: 'two', disabled: true },
+    { id: 'three', label: 'three' },
+    { id: 'fourth', label: 'fourth' },
+  ],
+  currentId: 'one',
+};
---
+++ packages/react-components/src/components/SegmentedControl/SegmentedControl.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import cx from 'clsx';
+
+import { Button, ButtonProps } from '../Button';
+
+import styles from './SegmentedControl.module.scss';
+
+import noop from '../../utils/noop';
+
+const baseClass = 'segmented-control';
+
+export type ButtonSize = 'compact' | 'medium' | 'large';
+
+type ButtonElement = {
+  id: string;
+  label: string;
+} & Pick<ButtonProps, 'disabled' | 'loading'>;
+
+export interface SegmentedControlProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+  buttons: ButtonElement[];
+  fullWidth?: boolean;
+  size?: ButtonSize;
+  initialId?: string;
+  currentId?: string;
+  onButtonClick?: (id: string, event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const SegmentedControl: React.FC<SegmentedControlProps> = ({
+  size = 'medium',
+  buttons,
+  className,
+  initialId,
+  currentId,
+  fullWidth = false,
+  onButtonClick = noop,
+}) => {
+  const mergedClassName = cx(styles[baseClass], className);
+  const [currentStateId, setCurrentStateId] = React.useState(() => initialId);
+
+  const isControlled = typeof currentId === 'string';
+
+  React.useEffect(() => {
+    isControlled && setCurrentStateId(currentId);
+  }, [currentId]);
+
+  const handleClick = (id: string, event: any) => {
+    if (!isControlled) {
+      setCurrentStateId(id);
+    }
+
+    onButtonClick(id, event);
+  };
+  const buttonSet = buttons.map(({ id, label, loading, disabled }) => {
+    const activityStyles = id === currentStateId ? styles['btn--active'] : '';
+    const loadingStatus = id === currentStateId ? false : loading;
+
+    return (
+      <Button
+        key={id}
+        fullWidth={fullWidth}
+        loading={loadingStatus}
+        disabled={disabled}
+        kind="secondary"
+        className={cx(styles['btn'], styles[`btn--${size}`], activityStyles)}
+        onClick={(event: React.MouseEvent<HTMLElement>) => {
+          handleClick(id, event);
+        }}
+      >
+        {label}
+      </Button>
+    );
+  });
+
+  return (
+    <div role="group" className={mergedClassName}>
+      {buttonSet}
+    </div>
+  );
+};
