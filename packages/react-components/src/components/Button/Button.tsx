6f4617cecf640a8c9350e5c7b2d039c66a2093f6
Marcin Sawicki <sawicki.marcin.88@gmail.com>
2023-04-13 09:52:14 +0200

    Revert "Ref for button component (#516)"
    This reverts commit 6e31b5f785b7a53871e657adc7a119f236cc2aa9.

--- packages/react-components/src/components/Button/Button.tsx
+++ packages/react-components/src/components/Button/Button.tsx
@@ -6,10 +6,8 @@ import {
   ReactElement,
   ButtonHTMLAttributes,
   AnchorHTMLAttributes,
+  FC,
   cloneElement,
-  forwardRef,
-  ExoticComponent,
-  RefAttributes,
 } from 'react';
 
 export type ButtonKind =
@@ -34,86 +32,78 @@ export type ButtonProps = {
 
 const baseClass = 'btn';
 
-export const Button: ExoticComponent<
-  ButtonProps & RefAttributes<HTMLButtonElement & HTMLAnchorElement>
-> = forwardRef(
-  (
+export const Button: FC<ButtonProps> = ({
+  loading = false,
+  disabled = false,
+  type = 'button',
+  fullWidth = false,
+  kind = 'basic',
+  size = 'medium',
+  icon = null,
+  iconPosition = 'left',
+  loaderLabel,
+  className,
+  children,
+  href,
+  onClick,
+  ...props
+}) => {
+  const isDisabled = loading || disabled;
+  const isIconOnly = !children && icon;
+  const isTextButton = ['text', 'plain', 'plain-light'].includes(kind);
+
+  const Component = href ? 'a' : 'button';
+
+  const getSpinnerColors = () => {
+    if (kind === 'primary' || kind === 'destructive') {
+      return {
+        primaryColor: 'var(--action-primary-default)',
+        secondaryColor: 'var(--border-invert-primary)',
+      };
+    }
+  };
+
+  const mergedClassNames = cx(
+    className,
+    styles[baseClass],
+    styles[`${baseClass}--${kind}`],
+    styles[`${baseClass}--${size}`],
     {
-      loading = false,
-      disabled = false,
-      type = 'button',
-      fullWidth = false,
-      kind = 'basic',
-      size = 'medium',
-      icon = null,
-      iconPosition = 'left',
-      loaderLabel,
-      className,
-      children,
-      href,
-      onClick,
-      ...props
-    },
-    ref
-  ) => {
-    const isDisabled = loading || disabled;
-    const isIconOnly = !children && icon;
-    const isTextButton = ['text', 'plain', 'plain-light'].includes(kind);
+      [styles[`${baseClass}--loading`]]: loading,
+      [styles[`${baseClass}--full-width`]]: fullWidth,
+      [styles[`${baseClass}--icon-only`]]: isIconOnly,
+      [styles[`${baseClass}--icon-only--bg`]]: isIconOnly && isTextButton,
+      [styles[`${baseClass}--disabled`]]: isDisabled,
+    }
+  );
 
-    const Component = href ? 'a' : 'button';
-
-    const getSpinnerColors = () => {
-      if (kind === 'primary' || kind === 'destructive') {
-        return {
-          primaryColor: 'var(--action-primary-default)',
-          secondaryColor: 'var(--border-invert-primary)',
-        };
-      }
-    };
-
-    const mergedClassNames = cx(
-      className,
-      styles[baseClass],
-      styles[`${baseClass}--${kind}`],
-      styles[`${baseClass}--${size}`],
-      {
-        [styles[`${baseClass}--loading`]]: loading,
-        [styles[`${baseClass}--full-width`]]: fullWidth,
-        [styles[`${baseClass}--icon-only`]]: isIconOnly,
-        [styles[`${baseClass}--icon-only--bg`]]: isIconOnly && isTextButton,
-        [styles[`${baseClass}--disabled`]]: isDisabled,
-      }
-    );
-
-    return (
-      <Component
-        ref={ref}
-        className={mergedClassNames}
-        aria-disabled={isDisabled}
-        type={type}
-        href={isDisabled ? undefined : href}
-        onClick={isDisabled ? undefined : onClick}
-        disabled={isDisabled}
-        {...props}
-      >
-        {loading && (
-          <Loader
-            size="small"
-            label={loaderLabel}
-            className={styles[`${baseClass}__loader`]}
-            {...getSpinnerColors()}
-          />
-        )}
-        {icon &&
-          cloneElement(icon, {
-            className: cx(
-              styles[`${baseClass}__icon`],
-              styles[`${baseClass}__icon--${iconPosition}`]
-            ),
-            disabled,
-          })}
-        <div className={styles[`${baseClass}__content`]}>{children}</div>
-      </Component>
-    );
-  }
-);
+  return (
+    <Component
+      className={mergedClassNames}
+      aria-disabled={isDisabled}
+      type={type}
+      href={isDisabled ? undefined : href}
+      onClick={isDisabled ? undefined : onClick}
+      disabled={isDisabled}
+      {...props}
+    >
+      {loading && (
+        <Loader
+          size="small"
+          label={loaderLabel}
+          className={styles[`${baseClass}__loader`]}
+          {...getSpinnerColors()}
+        />
+      )}
+      {icon &&
+        cloneElement(icon, {
+          className: cx(
+            styles[`${baseClass}__icon`],
+            styles[`${baseClass}__icon--${iconPosition}`]
+          ),
+          disabled,
+        })}
+      <div className={styles[`${baseClass}__content`]}>{children}</div>
+    </Component>
+  );
+};
