517edce693fd6aacb18597e1e72f90d07d4415a7
Marcin Sawicki <m.sawicki@livechat.com>
2023-04-19 13:40:35 +0200

    ActionMenu with defined items, keyboard functionality, UI

--- packages/react-components/src/components/ActionMenu/ActionMenu.stories.tsx
+++ packages/react-components/src/components/ActionMenu/ActionMenu.stories.tsx
@@ -1,24 +1,64 @@
 import { ComponentMeta } from '@storybook/react';
-import { MoreHoriz } from '@livechat/design-system-icons/react/tabler';
+import {
+  MoreHoriz,
+  AccountCircle,
+  Add,
+  ArrowForward,
+  Block,
+  ChatDots,
+} from '@livechat/design-system-icons/react/tabler';
 import { Icon } from '../Icon';
 import { ActionMenu, ActionMenuProps } from './ActionMenu';
+
+import './ActionMenu.stories.css';
+import noop from '../../utils/noop';
 
 const items = [
   {
     key: 'one',
-    element: <div>Option one</div>,
-    onClick: () => console.log('one'),
+    element: (
+      <>
+        <Icon source={AccountCircle} className="menu-item-icon" /> Option one
+      </>
+    ),
+    onClick: noop,
   },
   {
     key: 'two',
-    element: <div>Option two</div>,
-    disabled: true,
-    onClick: () => console.log('two'),
+    element: (
+      <>
+        <Icon source={Add} className="menu-item-icon" /> Option two
+      </>
+    ),
+    onClick: noop,
   },
   {
     key: 'three',
-    element: <div>Option three</div>,
-    onClick: () => console.log('three'),
+    element: (
+      <>
+        <Icon source={ArrowForward} className="menu-item-icon" /> Option three
+      </>
+    ),
+    onClick: noop,
+  },
+  {
+    key: 'four',
+    element: (
+      <>
+        <Icon source={Block} className="menu-item-icon" /> Option four
+      </>
+    ),
+    onClick: noop,
+    disabled: true,
+  },
+  {
+    key: 'five',
+    element: (
+      <>
+        <Icon source={ChatDots} className="menu-item-icon" /> Option five
+      </>
+    ),
+    onClick: noop,
     withDivider: true,
   },
 ];
@@ -26,11 +66,14 @@ const items = [
 export default {
   title: 'Components/ActionMenu',
   component: ActionMenu,
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
 } as ComponentMeta<typeof ActionMenu>;
 
 export const Default = (args: ActionMenuProps): JSX.Element => (
-  <div style={{ marginLeft: 200 }}>
-    <ActionMenu {...args} />
+  <div style={{ marginLeft: 200, marginBottom: 220 }}>
+    <ActionMenu data-testid={'test id'} {...args} />
   </div>
 );
 Default.args = {
--- packages/react-components/src/components/ActionMenu/ActionMenu.tsx
+++ packages/react-components/src/components/ActionMenu/ActionMenu.tsx
@@ -1,8 +1,15 @@
-import { FC, ReactNode, ReactElement } from 'react';
+import {
+  FC,
+  ReactNode,
+  ReactElement,
+  useState,
+  useEffect,
+  useRef,
+} from 'react';
 import cx from 'clsx';
 import { Popover } from '../Popover';
 import styles from './ActionMenu.module.scss';
-import { useState } from 'react';
+import { KeyCodes } from '../../utils/keyCodes';
 
 export interface ActionMenuItemsProps {
   key: string;
@@ -13,6 +20,7 @@ export interface ActionMenuItemsProps {
 }
 
 export interface ActionMenuProps {
+  className?: string;
   options: ActionMenuItemsProps[];
   triggerRenderer: ReactNode;
 }
@@ -20,17 +28,53 @@ export interface ActionMenuProps {
 const baseClass = 'action-menu';
 
 export const ActionMenu: FC<ActionMenuProps> = ({
+  className,
   options,
   triggerRenderer,
+  ...props
 }) => {
   const [isVisible, setIsVisible] = useState(false);
+  const indexRef = useRef(-1);
+
+  const getIndex = (val: number) => {
+    indexRef.current = indexRef.current + val;
+
+    while (options[indexRef.current].disabled) {
+      indexRef.current = indexRef.current + val;
+    }
+
+    return indexRef.current;
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === KeyCodes.arrowUp && indexRef.current > 0) {
+      e.preventDefault();
+      indexRef.current = getIndex(-1);
+      document.getElementById(`list-item-${indexRef.current}`)?.focus();
+    }
+
+    if (e.key === KeyCodes.arrowDown && indexRef.current + 1 < options.length) {
+      e.preventDefault();
+      indexRef.current = getIndex(+1);
+      document.getElementById(`list-item-${indexRef.current}`)?.focus();
+    }
+  };
+
+  useEffect(() => {
+    if (isVisible) {
+      document.addEventListener('keydown', onKeyDown);
+      return () => document.removeEventListener('keydown', onKeyDown);
+    } else {
+      indexRef.current = -1;
+    }
+  }, [isVisible, onKeyDown]);
 
   const handleTriggerClick = () => {
     setIsVisible(true);
   };
 
-  const handleItemClick = (onClick) => {
-    onClick();
+  const handleItemClick = (itemOnClick: () => void) => {
+    itemOnClick();
     setIsVisible(false);
   };
 
@@ -48,20 +92,28 @@ export const ActionMenu: FC<ActionMenuProps> = ({
         </button>
       )}
     >
-      <ul className={styles[`${baseClass}__list`]} role="menu">
-        {options.map((i) => (
-          <li role="none">
+      <ul
+        {...props}
+        className={cx(styles[`${baseClass}__list`], className)}
+        role="menu"
+      >
+        {options.map((o, i) => (
+          <li key={o.key} role="none">
             <button
-              disabled={i.disabled}
-              onClick={() => handleItemClick(i.onClick)}
+              id={`list-item-${i}`}
+              data-testid={o.key}
+              tabIndex={-1}
+              key={o.key}
+              disabled={o.disabled}
+              onClick={() => handleItemClick(o.onClick)}
               role="menuitem"
               className={cx(styles[`${baseClass}__list__item`], {
-                [styles[`${baseClass}__list__item--disabled`]]: i.disabled,
+                [styles[`${baseClass}__list__item--disabled`]]: o.disabled,
                 [styles[`${baseClass}__list__item--with-divider`]]:
-                  i.withDivider,
+                  o.withDivider,
               })}
             >
-              {i.element}
+              {o.element}
             </button>
           </li>
         ))}
