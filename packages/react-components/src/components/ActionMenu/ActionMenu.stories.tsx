7ba62cbcb3fc04c1e5cbde35ee8520bd4656f8ad
Marcin Sawicki <m.sawicki@livechat.com>
2023-04-19 14:29:48 +0200

    chnges in documentation

--- packages/react-components/src/components/ActionMenu/ActionMenu.stories.tsx
+++ packages/react-components/src/components/ActionMenu/ActionMenu.stories.tsx
@@ -1,82 +1,53 @@
 import { ComponentMeta } from '@storybook/react';
-import {
-  MoreHoriz,
-  AccountCircle,
-  Add,
-  ArrowForward,
-  Block,
-  ChatDots,
-} from '@livechat/design-system-icons/react/tabler';
+import { MoreHoriz } from '@livechat/design-system-icons/react/tabler';
 import { Icon } from '../Icon';
-import { ActionMenu, ActionMenuProps } from './ActionMenu';
+import { ActionMenu } from './ActionMenu';
+import { ActionMenuItemsDocumentationProps, exampleOptions } from './constants';
 
 import './ActionMenu.stories.css';
-import noop from '../../utils/noop';
-
-const items = [
-  {
-    key: 'one',
-    element: (
-      <>
-        <Icon source={AccountCircle} className="menu-item-icon" /> Option one
-      </>
-    ),
-    onClick: noop,
-  },
-  {
-    key: 'two',
-    element: (
-      <>
-        <Icon source={Add} className="menu-item-icon" /> Option two
-      </>
-    ),
-    onClick: noop,
-  },
-  {
-    key: 'three',
-    element: (
-      <>
-        <Icon source={ArrowForward} className="menu-item-icon" /> Option three
-      </>
-    ),
-    onClick: noop,
-  },
-  {
-    key: 'four',
-    element: (
-      <>
-        <Icon source={Block} className="menu-item-icon" /> Option four
-      </>
-    ),
-    onClick: noop,
-    disabled: true,
-  },
-  {
-    key: 'five',
-    element: (
-      <>
-        <Icon source={ChatDots} className="menu-item-icon" /> Option five
-      </>
-    ),
-    onClick: noop,
-    withDivider: true,
-  },
-];
 
 export default {
   title: 'Components/ActionMenu',
   component: ActionMenu,
   argTypes: {
-    onClick: { action: 'clicked' },
+    className: {
+      description: 'The CSS class for menu container',
+      control: {
+        disable: true,
+      },
+    },
+    options: {
+      description: 'Array of menu options',
+      table: {
+        type: {
+          summary: 'ActionMenuItemsProps',
+          detail: ActionMenuItemsDocumentationProps,
+        },
+      },
+      control: {
+        disable: true,
+      },
+    },
+    triggerRenderer: {
+      description: 'Trigger element',
+      control: {
+        disable: true,
+      },
+    },
+    onClick: {
+      table: {
+        disable: true,
+      },
+      action: 'clicked',
+    },
   },
 } as ComponentMeta<typeof ActionMenu>;
 
-export const Default = (args: ActionMenuProps): JSX.Element => (
+export const Default = (): JSX.Element => (
   <div style={{ marginLeft: 200, marginBottom: 220 }}>
-    <ActionMenu data-testid={'test id'} {...args} />
+    <ActionMenu
+      options={exampleOptions}
+      triggerRenderer={<Icon source={MoreHoriz} kind="primary" />}
+    />
   </div>
 );
-Default.args = {
-  options: items,
-  triggerRenderer: <Icon source={MoreHoriz} kind="primary" />,
-};
