f2d185c256288970e5509ef06f4c76608c2a43d7
Bartek <44156725+bbcz@users.noreply.github.com>
2023-05-07 11:45:50 +0200

    added additional tests to the tooltip spec

--- packages/react-components/src/components/Tooltip/Tooltip.spec.tsx
+++ packages/react-components/src/components/Tooltip/Tooltip.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, fireEvent, cleanup } from 'test-utils';
+import { render, fireEvent, cleanup, act } from 'test-utils';
 import { vi } from 'vitest';
 
 import { Tooltip } from './Tooltip';
@@ -39,13 +39,115 @@ describe('<Tooltip> component', () => {
       </Tooltip>
     );
 
-    fireEvent.mouseOver(
-      getByRole('button', {
-        name: /Open/i,
-      })
+    act(() => {
+      fireEvent.mouseOver(getByRole('button', { name: 'Open' }));
+    });
+
+    expect(container).toHaveTextContent('Test');
+
+    cleanup();
+  });
+
+  it.skip(`should hide the content if we doesn't hover the button`, () => {
+    const { container, getByRole, getByText } = render(
+      <Tooltip
+        hoverOutDelayTimeout={0}
+        triggerRenderer={() => <button>Open</button>}
+      >
+        <div style={{ width: '100px' }}> Test </div>
+      </Tooltip>
+    );
+
+    act(() => {
+      fireEvent.mouseOver(getByRole('button', { name: 'Open' }));
+    });
+
+    expect(container).toHaveTextContent('Test');
+
+    act(() => {
+      fireEvent.mouseOut(getByRole('button', { name: 'Open' }));
+    });
+    expect(getByText('Test')).not.toBeVisible();
+    cleanup();
+  });
+
+  it(`shouldn't show content on mouse over when we set isVisible=false`, () => {
+    const { container, getByRole } = render(
+      <Tooltip isVisible={false} triggerRenderer={() => <button>Open</button>}>
+        <div style={{ width: '100px' }}> Test </div>
+      </Tooltip>
+    );
+
+    act(() => {
+      fireEvent.mouseOver(getByRole('button', { name: 'Open' }));
+    });
+
+    expect(container).not.toHaveTextContent('Test');
+    expect(container).toHaveTextContent('Open');
+
+    cleanup();
+  });
+
+  it(`should show content without mouse over`, () => {
+    const { container } = render(
+      <Tooltip isVisible={true} triggerRenderer={() => <button>Open</button>}>
+        <div style={{ width: '100px' }}> Test </div>
+      </Tooltip>
     );
 
     expect(container).toHaveTextContent('Test');
+    cleanup();
+  });
+
+  it(`should show content after click and disappear after second click`, () => {
+    const buttonClick = () =>
+      act(() => {
+        fireEvent.click(getByRole('button', { name: 'Open' }));
+      });
+
+    const { container, getByRole, getByText } = render(
+      <Tooltip
+        triggerOnClick={true}
+        triggerRenderer={() => <button>Open</button>}
+      >
+        <div style={{ width: '100px' }}> Test </div>
+      </Tooltip>
+    );
+
+    act(() => {
+      fireEvent.mouseOver(getByRole('button', { name: 'Open' }));
+    });
+    expect(container).not.toHaveTextContent('Test');
+
+    buttonClick();
+    expect(container).toHaveTextContent('Test');
+
+    buttonClick();
+    expect(getByText('Test')).not.toBeVisible();
+
+    buttonClick();
+    act(() => {
+      fireEvent.keyDown(getByText('Test'), { key: 'Escape', code: 'Escape' });
+    });
+    expect(getByText('Test')).not.toBeVisible();
+
+    cleanup();
+  });
+
+  it('should have proper theme atribute for important theme', () => {
+    const { getByTestId } = render(
+      <Tooltip
+        isVisible={true}
+        theme="important"
+        triggerRenderer={() => <button>Open</button>}
+      >
+        <div data-testid="import-tooltip" style={{ width: '100px' }}>
+          Color Test
+        </div>
+      </Tooltip>
+    );
+
+    expect(getByTestId('import-tooltip')).toHaveAttribute('theme', 'important');
 
     cleanup();
   });
