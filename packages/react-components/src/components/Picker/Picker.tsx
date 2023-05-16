45856eff4c3fff70523fcdf1548c7109b7a310ac
Marcin Sawicki <m.sawicki@livechat.com>
2023-05-16 11:27:22 +0200

    added possibility to hide clear button, docs improvements

--- packages/react-components/src/components/Picker/Picker.tsx
+++ packages/react-components/src/components/Picker/Picker.tsx
@@ -16,20 +16,69 @@ const baseClass = 'picker';
 export type PickerType = 'single' | 'multi';
 
 export interface IPickerProps {
+  /**
+   * Specify the custom id
+   */
   id?: string;
+  /**
+   * The CSS class for picker container
+   */
   className?: string;
+  /**
+   * Specify whether the picker should be disabled
+   */
   disabled?: boolean;
+  /**
+   * Specify whether the picker should be in error state
+   */
   error?: boolean;
+  /**
+   * Array of picker options
+   */
   options: IPickerListItem[];
+  /**
+   * Array of picker selected options
+   */
   selected?: IPickerListItem[] | null;
+  /**
+   * Specify the picker size
+   */
   size?: Size;
+  /**
+   * Set the dismiss icon size in tags when `multi` type is enabled
+   */
   tagIconSize?: IconSize;
+  /**
+   * Specify the placeholder for search input
+   */
   placeholder?: string;
+  /**
+   * Specify whether the option select is required
+   */
   isRequired?: boolean;
+  /**
+   * Text if no search result were found
+   */
   noSearchResultText?: string;
+  /**
+   * Text for `select all` option which will be visible if defined in multi select mode
+   */
   selectAllOptionText?: string;
+  /**
+   * Set `multi` to specify whether the picker should allow to multi selection
+   */
   type?: PickerType;
+  /**
+   * Set to disable search input
+   */
   searchDisabled?: boolean;
+  /**
+   * Set to hide clear selection button
+   */
+  hideClearButton?: boolean;
+  /**
+   * Callback called after item selection
+   */
   onSelect: (selectedItems: IPickerListItem[] | null) => void;
 }
 
@@ -47,6 +96,7 @@ export const Picker: React.FC<IPickerProps> = ({
   selectAllOptionText,
   type = 'single',
   searchDisabled = false,
+  hideClearButton,
   onSelect,
   ...props
 }) => {
@@ -192,6 +242,7 @@ export const Picker: React.FC<IPickerProps> = ({
           isRequired={isRequired}
           isMultiSelect={type === 'multi'}
           size={size}
+          hideClearButton={hideClearButton}
           onTrigger={handleTrigger}
           onClear={handleClear}
         >
