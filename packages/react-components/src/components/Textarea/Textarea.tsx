027d8d7c189a04327e01d0798df452f745bfe960
Mateusz Romek <mateusz.romek@outlook.com>
2023-05-29 07:40:51 +0200

    add onfocus and onblur

--- packages/react-components/src/components/Textarea/Textarea.tsx
+++ packages/react-components/src/components/Textarea/Textarea.tsx
@@ -13,7 +13,7 @@ export interface TextareaProps
 
 export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, error, ...textareaProps }, ref) => {
-    const { disabled } = textareaProps;
+    const { disabled, onBlur, onFocus } = textareaProps;
     const [isFocused, setIsFocused] = React.useState(false);
     const mergedClassNames = cx(className, styles[baseClass], {
       [styles[`${baseClass}--disabled`]]: disabled,
@@ -21,13 +21,27 @@ export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
       [styles[`${baseClass}--error`]]: error,
     });
 
+    const handleOnBlur = (
+      e: React.FocusEvent<HTMLTextAreaElement, Element>
+    ) => {
+      setIsFocused(false);
+      onBlur?.(e);
+    };
+
+    const handleOnFocus = (
+      e: React.FocusEvent<HTMLTextAreaElement, Element>
+    ) => {
+      setIsFocused(true);
+      onFocus?.(e);
+    };
+
     return (
       <div className={mergedClassNames}>
         <textarea
           {...textareaProps}
           ref={ref}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
+          onFocus={handleOnFocus}
+          onBlur={handleOnBlur}
         />
       </div>
     );
