7fa402da629b7b2a69656f1a1db3b105e492c73d
Marcin Sawicki <m.sawicki@livechatinc.com>
2023-06-20 13:24:19 +0200

    #555 - Progress - UI fixes + docs update (#606)
    * UI fixes + docs update
    
    * changes after review
    
    * changes after review

--- packages/react-components/src/components/Loader/Loader.stories.tsx
+++ packages/react-components/src/components/Loader/Loader.stories.tsx
@@ -3,23 +3,35 @@ import { ComponentMeta } from '@storybook/react';
 
 import { Loader, LoaderProps } from './Loader';
 
+import { StoryDescriptor } from '../../stories/components/StoryDescriptor';
+
 export default {
   title: 'Components/Loader',
   component: Loader,
 } as ComponentMeta<typeof Loader>;
 
-export const sizes = (): React.ReactElement => (
-  <div className="story-spacer">
-    <Loader size="small" />
-    <Loader size="medium" />
-    <Loader size="large" />
+export const Default = (args: LoaderProps): React.ReactElement => (
+  <Loader {...args} />
+);
+
+export const Sizes = (): React.ReactElement => (
+  <div>
+    <StoryDescriptor title="Small">
+      <Loader size="small" />
+    </StoryDescriptor>
+    <StoryDescriptor title="Medium">
+      <Loader size="medium" />
+    </StoryDescriptor>
+    <StoryDescriptor title="Large">
+      <Loader size="large" />
+    </StoryDescriptor>
   </div>
 );
 
-export const labeled = (args: LoaderProps): React.ReactElement => (
+export const Labeled = (args: LoaderProps): React.ReactElement => (
   <Loader size="small" label="Loading..." {...args} />
 );
 
-export const customColors = (args: LoaderProps): React.ReactElement => (
+export const CustomColors = (args: LoaderProps): React.ReactElement => (
   <Loader primaryColor="#d64646" secondaryColor="#eec4c5" {...args} />
 );
