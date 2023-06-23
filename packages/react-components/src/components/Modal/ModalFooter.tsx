a989d5e64ff691d0adadd3155257a53298380f15
jpyzio123 <46003125+jpyzio123@users.noreply.github.com>
2023-06-23 11:59:40 +0200

    css fixes

--- packages/react-components/src/components/Modal/ModalFooter.tsx
+++ packages/react-components/src/components/Modal/ModalFooter.tsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import cx from 'clsx';
-import styles from './Modal.module.scss';
-
-const baseClass = 'modal-footer';
-
 interface ModalFooterProps {
   children: React.ReactNode;
   className?: string;
@@ -13,6 +8,5 @@ export const ModalFooter: React.FC<ModalFooterProps> = ({
   children,
   className = '',
 }) => {
-  const mergedClassNames = cx(styles[baseClass], className);
-  return <div className={mergedClassNames}>{children}</div>;
+  return <div className={className}>{children}</div>;
 };
--- packages/react-components/src/components/Modal/ModalHeader.tsx
+++ packages/react-components/src/components/Modal/ModalHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Icon, IconSource } from '../Icon';
 import styles from './Modal.module.scss';
+import { cx } from '@emotion/css';
 
 const baseClass = 'modal-header';
 
@@ -8,15 +9,19 @@ interface ModalHeaderProps {
   title?: React.ReactNode;
   icon?: IconSource;
   children?: React.ReactNode;
+  className?: 'string';
 }
 
 export const ModalHeader: React.FC<ModalHeaderProps> = ({
   title,
   icon,
   children,
+  className = '',
 }) => {
+  const mergedClassNames = cx(styles[baseClass], className);
+
   return (
-    <div className="heading-wrapper">
+    <div className={mergedClassNames}>
       {icon && (
         <Icon className={styles[`${baseClass}__heading-icon`]} source={icon} />
       )}
