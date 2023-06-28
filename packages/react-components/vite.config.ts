8006584b763a14dcb0341ed088a975e69139fe10
Vadym <v.bezpalko@livechat.com>
2023-06-28 11:33:28 +0200

    updating vite-plugin-dts and types path in package.json

--- packages/react-components/vite.config.ts
+++ packages/react-components/vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
     },
   },
   plugins: [
-    dts({ logLevel: 'error' }),
+    dts(),
     react({ jsxRuntime: 'classic' }),
     turbosnap({ rootDir: process.cwd() }),
   ],
