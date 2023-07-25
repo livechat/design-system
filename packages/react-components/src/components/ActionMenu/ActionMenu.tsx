c7d5dcd66886ee6dbd553f2d54052f2a2ef5e90d
Marcin Sawicki <m.sawicki@livechatinc.com>
2023-07-25 10:54:29 +0200

    #566 - ActionMenu - Option to prevent closing when option is selected (#678)
    * option to prevent closing when option is selected
    
    * tests + fix
    
    * Update packages/react-components/src/components/ActionMenu/ActionMenu.tsx
    
    Co-authored-by: Vadym <12149393+VadymBezpalko@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Vadym <12149393+VadymBezpalko@users.noreply.github.com>

--- packages/react-components/src/components/ActionMenu/ActionMenu.tsx
+++ packages/react-components/src/components/ActionMenu/ActionMenu.tsx
@@ -32,6 +32,10 @@ export interface ActionMenuProps {
    * Will open menu on component initialization
    */
   openedOnInit?: boolean;
+  /**
+   * Menu will stay open after option click
+   */
+  keepOpenOnClick?: boolean;
 }
 
 const baseClass = 'action-menu';
@@ -42,6 +46,7 @@ export const ActionMenu: React.FC<ActionMenuProps> = ({
   triggerRenderer,
   placement = 'bottom-end',
   openedOnInit = false,
+  keepOpenOnClick,
   ...props
 }) => {
   const [isVisible, setIsVisible] = React.useState(openedOnInit);
@@ -86,14 +91,16 @@ export const ActionMenu: React.FC<ActionMenuProps> = ({
 
   const handleItemClick = (itemOnClick: () => void) => {
     itemOnClick();
-    setIsVisible(false);
+
+    if (!keepOpenOnClick) {
+      setIsVisible(false);
+    }
   };
 
   return (
     <Popover
       isVisible={isVisible}
       placement={placement}
-      onClose={() => setIsVisible(false)}
       triggerRenderer={() => (
         <button
           data-testid="action-menu-trigger-button"
