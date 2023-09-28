f6b2b314fde40d9520c7d4938559842de137685a
Vadym <v.bezpalko@livechat.com>
2023-09-28 10:19:42 +0200

    reducing the number of supported formats for components library

--- packages/react-components/vite.config.ts
+++ packages/react-components/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'dsrc',
-      formats: ['es', 'umd', 'cjs'],
+      formats: ['es'],
       fileName: (format) => `dsrc.${format}.js`,
     },
     rollupOptions: {
