be24d1c27280d0f774adab7dc00915001817f8a3
Vadym <v.bezpalko@livechat.com>
2023-10-06 14:41:00 +0200

    tree-shakable icons

---
+++ packages/icons/scripts/updateExports.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+function updateExports() {
+  // Read the files in the dist directory
+  const files = fs.readdirSync(path.resolve(__dirname, '../dist'));
+
+  // Initialize the exports object with the default entry
+  const exports = {
+    '.': './dist/index.js',
+  };
+
+  // Create a Set to store the module names (to prevent duplicate entries)
+  const moduleNames = new Set();
+
+  // Loop through the files, collecting module names and filtering out non-JS files
+  for (const file of files) {
+    const ext = path.extname(file);
+    if (ext === '.js') {
+      const moduleName = path.basename(file, ext);
+      // Separate the module name and the module format (es/cjs)
+      const [name, format] = moduleName.split('.');
+      // Add the module name to the Set
+      moduleNames.add(name);
+    }
+  }
+
+  // Loop through the unique module names, creating an entry for each one
+  for (const name of moduleNames) {
+    exports[`./${name}`] = {
+      import: `./dist/${name}.es.js`,
+      require: `./dist/${name}.cjs.js`,
+    };
+  }
+
+  // Read the current package.json file
+  const packageJsonPath = path.resolve(__dirname, '../package.json');
+  const packageJson = require(packageJsonPath);
+
+  // Update the exports field and write the package.json file back to disk
+  packageJson.exports = exports;
+  fs.writeFileSync(
+    packageJsonPath,
+    JSON.stringify(packageJson, null, 2),
+    'utf8'
+  );
+}
+
+updateExports();
--- packages/icons/vite.config.ts
+++ packages/icons/vite.config.ts
@@ -4,23 +4,23 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
+import entryConfig from './entryConfig.js';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [dts({ include: ['lib'] })],
   build: {
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, 'lib/index.ts'),
+      entry: {
+        index: resolve(__dirname, 'lib/index.ts'),
+        ...entryConfig,
+      },
       formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format}.js`,
+      fileName: (format, entryName) => `${entryName}.${format}.js`,
     },
     rollupOptions: {
       external: ['react', 'react/jsx-runtime'],
-      output: {
-        preserveModules: true,
-        preserveModulesRoot: 'lib',
-        entryFileNames: '[name].js',
-      },
     },
   },
 });
