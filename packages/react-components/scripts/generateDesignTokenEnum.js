0d6084e33c69d65189c9a05c508b7fb796028f5b
Vadym <v.bezpalko@livechat.com>
2023-10-19 18:23:34 +0200

    feat(DesignToken): add a script to generate design-token.ts file based on scss styles; update for design-token.ts tokens

---
+++ packages/react-components/scripts/generateDesignTokenEnum.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+
+const scssFolder = './src/themes/';
+
+function pascalCase(str) {
+  return str
+    .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase())
+    .replace(/^(.)/, (m, chr) => chr.toUpperCase());
+}
+
+const keysSet = new Set();
+
+const files = fs
+  .readdirSync(scssFolder)
+  .filter((file) => file.endsWith('.scss'));
+
+for (const file of files) {
+  const contents = fs.readFileSync(path.join(scssFolder, file), 'utf8');
+  const matches = contents.match(/--[\w-]+/g) || [];
+  matches.forEach((key) => keysSet.add(key));
+}
+
+const keys = Array.from(keysSet);
+
+let allKeysPresent = true;
+
+for (const file of files) {
+  const contents = fs.readFileSync(path.join(scssFolder, file), 'utf8');
+  for (const key of keys) {
+    if (!contents.includes(key)) {
+      console.error(`Missing key ${key} in file ${file}`);
+      allKeysPresent = false;
+    }
+  }
+}
+
+if (allKeysPresent) {
+  const output = `export const DesignToken = {
+  ${keys.map((key) => `${pascalCase(key.slice(2))}: '${key}',`).join('\n  ')}
+};
+`;
+
+  fs.writeFileSync('./src/themes/design-token.ts', output);
+  console.log('TS file generated successfully');
+}
