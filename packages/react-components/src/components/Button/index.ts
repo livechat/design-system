ec241faf8839096366b5d592912434c65b78d215
Vadym <12149393+VadymBezpalko@users.noreply.github.com>
2023-10-25 12:22:10 +0200

    feat(Button): Add new link-inverted button kind (#859)

--- packages/react-components/src/components/Button/index.ts
+++ packages/react-components/src/components/Button/index.ts
@@ -1,2 +1,3 @@
 export { Button } from './Button';
-export type { ButtonKind, ButtonProps } from './Button';
+export type { ButtonProps } from './Button';
+export type { ButtonKind } from './types';
