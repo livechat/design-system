d4276968ac80ea9025c5722100dcfecaa21a8859
Vadym <v.bezpalko@livechat.com>
2023-11-03 11:15:33 +0100

    feat(Popover): tests

--- packages/react-components/src/components/Popover/Popover.spec.tsx
+++ packages/react-components/src/components/Popover/Popover.spec.tsx
@@ -1,41 +1,74 @@
 import * as React from 'react';
 
+import { vi } from 'vitest';
+
 import { render, fireEvent } from 'test-utils';
 
 import { Popover } from './Popover';
 
-const Content = () => {
-  return <div>Content</div>;
-};
-
-const Trigger = () => {
-  return <button>Button</button>;
-};
+const triggerButtonId = 'popover-trigger-button';
 
 describe('<Popover> component', () => {
-  it('should show content if isVisible is set', () => {
-    const { container } = render(
-      <Popover isVisible={true} triggerRenderer={Trigger}>
-        <Content></Content>
-      </Popover>
-    );
+  const props = {
+    triggerRenderer: <button>Open Popover</button>,
+    onClose: vi.fn(),
+    onOpen: vi.fn(),
+    children: 'Popover Content',
+  };
 
-    expect(container).toHaveTextContent('Content');
+  it('renders the trigger button', () => {
+    const { getByTestId } = render(<Popover {...props} />);
+    const triggerButton = getByTestId(triggerButtonId);
+    expect(triggerButton).toBeInTheDocument();
   });
 
-  it('should show content if trigger has been clicked', () => {
-    const { container, getByRole } = render(
-      <Popover triggerRenderer={Trigger}>
-        <Content></Content>
-      </Popover>
-    );
+  it('opens the popover when the trigger button is clicked', () => {
+    const { getByTestId, getByText } = render(<Popover {...props} />);
+    const triggerButton = getByTestId(triggerButtonId);
+    fireEvent.click(triggerButton);
+    const popoverContent = getByText('Popover Content');
+    expect(popoverContent).toBeInTheDocument();
+  });
 
-    fireEvent.click(
-      getByRole('button', {
-        name: /Button/i,
-      })
-    );
+  it('calls onOpen when the popover is opened', () => {
+    const { getByTestId } = render(<Popover {...props} />);
+    const triggerButton = getByTestId(triggerButtonId);
+    fireEvent.click(triggerButton);
+    expect(props.onOpen).toHaveBeenCalled();
+  });
 
-    expect(container).toHaveTextContent('Content');
+  it('calls onClose when the popover is closed', () => {
+    const { getByTestId } = render(<Popover {...props} openedOnInit={true} />);
+    const triggerButton = getByTestId(triggerButtonId);
+    fireEvent.click(triggerButton);
+    expect(props.onClose).toHaveBeenCalled();
+  });
+
+  it('closes the popover when Escape key is pressed', () => {
+    const { queryByText } = render(<Popover {...props} openedOnInit={true} />);
+    fireEvent.keyDown(document, { key: 'Escape' });
+    const popoverContent = queryByText('Popover Content');
+    expect(popoverContent).not.toBeInTheDocument();
+  });
+
+  it('does not close the popover when Escape key is pressed if closeOnEsc is false', () => {
+    const { getByText } = render(
+      <Popover {...props} openedOnInit={true} closeOnEsc={false} />
+    );
+    fireEvent.keyDown(document, { key: 'Escape' });
+    const popoverContent = getByText('Popover Content');
+    expect(popoverContent).toBeInTheDocument();
+  });
+
+  it('renders the popover as open when isVisible prop is true', () => {
+    const { getByText } = render(<Popover {...props} isVisible={true} />);
+    const popoverContent = getByText('Popover Content');
+    expect(popoverContent).toBeInTheDocument();
+  });
+
+  it('renders the popover as closed when isVisible prop is false', () => {
+    const { queryByText } = render(<Popover {...props} isVisible={false} />);
+    const popoverContent = queryByText('Popover Content');
+    expect(popoverContent).not.toBeInTheDocument();
   });
 });
