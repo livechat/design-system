7e19a5c9f3406b4706bd9b44a682a3e2b56c32e8
Marcin Sawicki <m.sawicki@livechat.com>
2023-11-29 15:31:18 +0100

    new tokens

---
+++ packages/react-components/src/foundations/transition-token.ts
@@ -0,0 +1,8 @@
+export const TransitionDurationToken = {
+  Fast: '--transition-duration-fast',
+};
+
+export const TransitionTimingToken = {
+  EaseIn: '--transition-timing-ease-in',
+  EaseOut: '--transition-timing-ease-out',
+};
