8830da42a2757c7cdf9436869b202b1335fc8f16
Vadym <v.bezpalko@livechat.com>
2023-12-14 13:47:23 +0100

    feat(Picker): cx import path fix

--- packages/react-components/src/components/Picker/Picker.tsx
+++ packages/react-components/src/components/Picker/Picker.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import { cx } from '@emotion/css';
 import {
   autoUpdate,
   flip,
@@ -17,6 +16,7 @@ import {
   useListNavigation,
   useRole,
 } from '@floating-ui/react';
+import cx from 'clsx';
 import * as ReactDOM from 'react-dom';
 
 import { PickerList } from './components/PickerList';
