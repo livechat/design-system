57098007f25749744196252409713e3e48c8cbf5
Vadym <12149393+VadymBezpalko@users.noreply.github.com>
2023-12-20 12:16:30 +0100

    feat(Icon): new adding new icons; moving a svgr config to a separate file (#930)

---
+++ packages/icons/.svgrrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  replaceAttrValues: {
+    '#424D57': 'currentcolor',
+    '#1B1B20': 'currentcolor',
+    '#000': 'currentcolor',
+    black: 'currentcolor',
+  },
+  typescript: true,
+  dimensions: false,
+  outDir: './lib',
+};
