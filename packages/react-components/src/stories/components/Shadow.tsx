e16907317c44a9c315d0c42e40b7403a8fbdc54d
Ash <82605611+ashbork@users.noreply.github.com>
2024-01-09 13:56:47 +0100

    use full names of tokens
    Co-authored-by: Vadym <12149393+VadymBezpalko@users.noreply.github.com>

--- packages/react-components/src/stories/components/Shadow.tsx
+++ packages/react-components/src/stories/components/Shadow.tsx
@@ -12,82 +12,82 @@ export const Shadow = (): React.ReactElement => {
           <th className="second-column">Use case</th>
         </tr>
         <tr>
-          <td>Float</td>
+          <td>ShadowToken.Float</td>
           <td>--shadow-float</td>
           <td>Cards, buttons, other elevated elements</td>
         </tr>
         <tr>
-          <td>PopOver</td>
+          <td>ShadowToken.PopOver</td>
           <td>--shadow-pop-over</td>
           <td>Popovers</td>
         </tr>
         <tr>
-          <td>Modal</td>
+          <td>ShadowToken.Modal</td>
           <td>--shadow-modal</td>
           <td>Modals</td>
         </tr>
         <tr>
-          <td>Tooltip</td>
+          <td>ShadowToken.Tooltip</td>
           <td>--shadow-tooltip</td>
           <td>Tooltips</td>
         </tr>
         <tr>
-          <td>TooltipArrowBottom</td>
+          <td>ShadowToken.TooltipArrowBottom</td>
           <td>--shadow-tooltip-arrow-bottom</td>
           <td>Tooltip arrow pointing down</td>
         </tr>
         <tr>
-          <td>TooltipArrowTop</td>
+          <td>ShadowToken.TooltipArrowTop</td>
           <td>--shadow-tooltip-arrow-top</td>
           <td>Tooltip arrow pointing up</td>
         </tr>
         <tr>
-          <td>TooltipArrowRight</td>
+          <td>ShadowToken.TooltipArrowRight</td>
           <td>--shadow-tooltip-arrow-right</td>
           <td>Tooltip arrow pointing right</td>
         </tr>
         <tr>
-          <td>TooltipArrowLeft</td>
+          <td>ShadowToken.TooltipArrowLeft</td>
           <td>--shadow-tooltip-arrow-left</td>
           <td>Tooltip arrow pointing left</td>
         </tr>
         <tr>
-          <td>Focus</td>
+          <td>ShadowToken.Focus</td>
           <td>--shadow-focus</td>
           <td>Focus indicators</td>
         </tr>
         <tr>
-          <td>DividerBottom</td>
+          <td>ShadowToken.DividerBottom</td>
           <td>--shadow-divider-bottom</td>
           <td>Separator on bottom of the element</td>
         </tr>
         <tr>
-          <td>DividerTop</td>
+          <td>ShadowToken.DividerTop</td>
           <td>--shadow-divider-top</td>
           <td>Separator on top of the element</td>
         </tr>
         <tr>
-          <td>DividerBottomLeft</td>
+          <td>ShadowToken.DividerBottomLeft</td>
           <td>--shadow-divider-bottom-left</td>
           <td>Separator on bottom left of the element</td>
         </tr>
         <tr>
-          <td>DividerTopLeft</td>
+          <td>ShadowToken.DividerTopLeft</td>
           <td>--shadow-divider-top-left</td>
           <td>Separator on top left of the element</td>
         </tr>
         <tr>
-          <td>DividerTopRight</td>
+          <td>ShadowToken.DividerTopRight</td>
           <td>--shadow-divider-top-right</td>
           <td>Separator on top right of the element</td>
         </tr>
         <tr>
-          <td>DividerBottomRight</td>
+          <td>ShadowToken.DividerBottomRight</td>
           <td>--shadow-divider-bottom-right</td>
           <td>Separator on bottom right of the element</td>
         </tr>
         <tr>
-          <td>MessageBox</td>
+          <td>ShadowToken.MessageBox</td>
           <td>--shadow-message-box</td>
           <td>Message boxes</td>
         </tr>
