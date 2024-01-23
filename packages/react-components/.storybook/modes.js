92b4aaec2393d3a481d40ce698f38176217143b3
Marcin Sawicki <m.sawicki@livechatinc.com>
2024-01-23 10:43:02 +0100

    Light/Dark theme visual tests in Chromatic (#978)

---
+++ packages/react-components/.storybook/modes.js
@@ -0,0 +1,10 @@
+export const allModes = {
+  dark: {
+    backgrounds: { value: '#131317' },
+    theme: 'dark',
+  },
+  light: {
+    backgrounds: { value: '#fff' },
+    theme: 'light',
+  },
+};
