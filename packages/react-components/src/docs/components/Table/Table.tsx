25c079c36e8de38a0d19829526d8b21424f54fd2
Vadym <v.bezpalko@livechat.com>
2024-01-26 16:05:14 +0100

    docs(ColorPage): shared table component

---
+++ packages/react-components/src/docs/components/Table/Table.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+
+interface ITable<T> {
+  data: T[];
+  columnNames: string[];
+  renderExample: (token: string) => React.ReactNode;
+}
+
+export const Table: React.FC<ITable<Record<string, unknown>>> = ({
+  data,
+  columnNames,
+  renderExample,
+}) => {
+  return (
+    <table className="sb-unstyled">
+      <thead>
+        <tr>
+          {columnNames.map((columnName) => (
+            <th>{columnName}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {Object.values(row).map((value, cellIndex) => {
+              if (cellIndex === 0) {
+                return (
+                  <td>
+                    {renderExample(row['value'] as string)}
+                    {row['enum']}
+                  </td>
+                );
+              }
+
+              if (typeof value === 'string') {
+                return <td key={cellIndex}>{value}</td>;
+              }
+            })}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
--- packages/react-components/src/docs/foundations/Color/ColorTable.tsx
+++ packages/react-components/src/docs/foundations/Color/ColorTable.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 
-import { handleCopyText } from '../../helpers';
+import { Table } from '../../components/Table/Table';
 
 import { ColorShape } from './types';
-
 import '../../components/Table/Table.css';
 
 interface IColorTableProps {
@@ -12,29 +11,15 @@ interface IColorTableProps {
 
 export const ColorTable: React.FC<IColorTableProps> = ({ data }) => {
   return (
-    <table className="sb-unstyled">
-      <thead>
-        <tr>
-          <th>Enum</th>
-          <th>Token</th>
-          <th>Usage</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((color) => (
-          <tr>
-            <td onClick={() => handleCopyText(color.enum)}>
-              <div
-                className="color-example"
-                style={{ backgroundColor: `var(${color.value})` }}
-              />
-              {color.enum}
-            </td>
-            <td>{color.value}</td>
-            <td>{color.desc}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <Table
+      data={data}
+      renderExample={(token) => (
+        <div
+          className="color-example"
+          style={{ backgroundColor: `var(${token})` }}
+        />
+      )}
+      columnNames={['Enum', 'Token', 'Usage']}
+    />
   );
 };
