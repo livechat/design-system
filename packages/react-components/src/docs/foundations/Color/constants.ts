28c9fcdebf45f64684d7bd7ebe1dd5b47cb8f2fd
Vadym <v.bezpalko@livechat.com>
2024-01-26 13:26:40 +0100

    docs(ColorPage): refactor

--- packages/react-components/src/docs/foundations/Color/constants.ts
+++ packages/react-components/src/docs/foundations/Color/constants.ts
@@ -2,13 +2,11 @@ import { DesignToken, DesignTokenKey } from '../../../themes/design-token';
 
 import { ColorGroup, ColorShape } from './types';
 
-const ColorBaseVariants = Object.entries(DesignToken).map(([key, value]) => ({
-  enum: key as DesignTokenKey,
-  value,
-}));
-
-export const ColorsTokens: ColorShape[] = ColorBaseVariants.map((color) => ({
-  ...color,
-  desc: 'Temporary description',
-  group: ColorGroup.Other,
-})).sort((a, b) => a.enum.localeCompare(b.enum));
+export const ColorsTokens: ColorShape[] = Object.entries(DesignToken)
+  .map(([key, value]) => ({
+    enum: key as DesignTokenKey,
+    value,
+    desc: 'Temporary description',
+    group: ColorGroup.Other,
+  }))
+  .sort((a, b) => a.enum.localeCompare(b.enum));
