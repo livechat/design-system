ff45f841275d50d6017fde6a2b11d69ee0e678f7
Vadym <v.bezpalko@livechat.com>
2024-01-29 14:57:45 +0100

    docs(Foundations): transition descriptions

--- packages/react-components/src/stories/foundations/Transition/types.ts
+++ packages/react-components/src/stories/foundations/Transition/types.ts
@@ -1,6 +1,24 @@
+import {
+  TransitionDelayTokenKey,
+  TransitionDurationTokenKey,
+  TransitionTimingTokenKey,
+} from '../../../foundations/transition-token';
+
 export type TransitionShape = {
   enum: string;
   token: string;
   value: string;
   desc: string;
+};
+
+export type TransitionDurationShape = TransitionShape & {
+  enum: TransitionDurationTokenKey;
+};
+
+export type TransitionTimingShape = TransitionShape & {
+  enum: TransitionTimingTokenKey;
+};
+
+export type TransitionDelayShape = TransitionShape & {
+  enum: TransitionDelayTokenKey;
 };
