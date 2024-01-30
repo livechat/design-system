dde98493af252d5304c650c1b5a1a4eaef9350c7
Vadym <v.bezpalko@livechat.com>
2024-01-30 15:03:13 +0100

    docs(Foundations): add copy hint to table

--- packages/react-components/src/stories/components/Table/Table.tsx
+++ packages/react-components/src/stories/components/Table/Table.tsx
@@ -21,52 +21,58 @@ export const Table: React.FC<ITable> = ({
   renderExample,
 }) => {
   return (
-    <table className="sb-unstyled">
-      <thead>
-        <tr>
-          {columnNames.map((columnName) => (
-            <th>{columnName}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {Object.entries(row).map(([key, value], cellIndex) => {
-              if (key === 'enum') {
-                return (
-                  <td
-                    key={cellIndex}
-                    onClick={() => handleCopyText(value as string)}
-                  >
-                    <div className="example-field copy-text">
-                      {renderExample && renderExample(row['token'])}
-                      {row['enum']}
-                      {row['deprecated'] && <i> (deprecated)</i>}
-                    </div>
-                  </td>
-                );
-              }
-
-              if (key === 'token') {
-                return (
-                  <td
-                    key={cellIndex}
-                    className="no-wrap copy-text"
-                    onClick={() => handleCopyText(value as string)}
-                  >
-                    {value}
-                  </td>
-                );
-              }
-
-              if (typeof value === 'string') {
-                return <td key={cellIndex}>{value}</td>;
-              }
-            })}
+    <>
+      <p>
+        ℹ️ Click on the <b>Enum</b> or <b>Token</b> value to copy it to the
+        clipboard.
+      </p>
+      <table className="sb-unstyled">
+        <thead>
+          <tr>
+            {columnNames.map((columnName) => (
+              <th>{columnName}</th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {Object.entries(row).map(([key, value], cellIndex) => {
+                if (key === 'enum') {
+                  return (
+                    <td
+                      key={cellIndex}
+                      onClick={() => handleCopyText(value as string)}
+                    >
+                      <div className="example-field copy-text">
+                        {renderExample && renderExample(row['token'])}
+                        {row['enum']}
+                        {row['deprecated'] && <i> (deprecated)</i>}
+                      </div>
+                    </td>
+                  );
+                }
+
+                if (key === 'token') {
+                  return (
+                    <td
+                      key={cellIndex}
+                      className="no-wrap copy-text"
+                      onClick={() => handleCopyText(value as string)}
+                    >
+                      {value}
+                    </td>
+                  );
+                }
+
+                if (typeof value === 'string') {
+                  return <td key={cellIndex}>{value}</td>;
+                }
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
