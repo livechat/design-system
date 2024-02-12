8585da745f56e50c018dba3373397de859835203
Vadym <v.bezpalko@livechat.com>
2024-02-12 12:38:27 +0100

    build(Vite): disable lib-inject-css for storybook builds

--- packages/react-components/.storybook/main.js
+++ packages/react-components/.storybook/main.js
@@ -1,4 +1,5 @@
 import { dirname, join } from 'path';
+import { withoutVitePlugins } from '@storybook/builder-vite';
 module.exports = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -18,6 +19,14 @@ module.exports = {
   },
   docs: {
     autodocs: true,
+  },
+  viteFinal: async (config) => {
+    return {
+      ...config,
+      plugins: await withoutVitePlugins(config.plugins, [
+        'vite:lib-inject-css',
+      ]),
+    };
   },
 };
 /**
