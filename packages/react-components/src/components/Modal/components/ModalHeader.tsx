baeda381c8dc89b6c3b2c2ef4b3cffb7292052f4
Marcin Sawicki <m.sawicki@livechat.com>
2024-02-27 09:36:14 +0100

    fix(Typography): tokens updates, modal and card components header changes

--- packages/react-components/src/components/Modal/components/ModalHeader.tsx
+++ packages/react-components/src/components/Modal/components/ModalHeader.tsx
@@ -57,11 +57,7 @@ export const ModalHeader: React.FC<ModalHeaderProps> = ({
         />
       )}
       <div className={styles[`${baseClass}__heading-body`]}>
-        <Heading
-          as="div"
-          size="md"
-          className={styles[`${baseClass}__heading-title`]}
-        >
+        <Heading as="div" size="sm">
           {title}
         </Heading>
 
