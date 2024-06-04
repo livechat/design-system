ea26a2910b25abd1d1b128a3e1c318267e67e1db
Vadym <v.bezpalko@livechat.com>
2024-06-04 13:00:49 +0200

    fix(Storybook): removing absolute path helper from main.js

--- packages/react-components/.storybook/main.js
+++ packages/react-components/.storybook/main.js
@@ -3,16 +3,16 @@ import { dirname, join } from 'path';
 module.exports = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-a11y'),
-    getAbsolutePath('@storybook/addon-designs'),
-    getAbsolutePath('@storybook/addon-mdx-gfm'),
-    getAbsolutePath('@storybook/addon-themes'),
-    getAbsolutePath('@storybook/addon-backgrounds'),
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-designs',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-themes',
+    '@storybook/addon-backgrounds',
   ],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: '@storybook/react-vite',
     options: {
       legacyRootApi: true, // TODO: remove when local React will be migrated to v18
     },
@@ -21,10 +21,3 @@ module.exports = {
     autodocs: true,
   },
 };
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
