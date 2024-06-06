2e7e3139fbdba79a6309ce83bbdb68afc7736552
Vadym <v.bezpalko@livechat.com>
2024-06-06 09:40:53 +0200

    fix(Storybook): converting main.js to TS format

--- packages/react-components/.storybook/main.ts
+++ packages/react-components/.storybook/main.ts
@@ -1,6 +1,7 @@
 import { dirname, join } from 'path';
+import { StorybookConfig } from '@storybook/react-vite';
 
-export default {
+const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
@@ -25,3 +26,5 @@ export default {
 function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, 'package.json')));
 }
+
+export default config;
