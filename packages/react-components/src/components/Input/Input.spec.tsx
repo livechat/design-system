824181f04e5e68ac9a5e6fd9527ddfa9d1647e74
Marcin Sawicki <m.sawicki@livechat.com>
2024-06-11 13:27:43 +0200

    fix(Input): tests updates

--- packages/react-components/src/components/Input/Input.spec.tsx
+++ packages/react-components/src/components/Input/Input.spec.tsx
@@ -2,54 +2,33 @@ import * as React from 'react';
 
 import { AddCircle as AddCircleIcon } from '@livechat/design-system-icons';
 
-import { fireEvent, render } from 'test-utils';
+import { render, userEvent, vi } from 'test-utils';
 
 import { Icon } from '../Icon';
 
-import { Input } from './Input';
+import { Input, InputProps } from './Input';
 
-import styles from './Input.module.scss';
+const renderComponent = (props: InputProps) => render(<Input {...props} />);
 
 describe('<Input> component', () => {
   it('should allow for custom class', () => {
-    const { container } = render(<Input className="my-css-class" />);
+    const { container } = renderComponent({ className: 'my-css-class' });
+
     expect(container.firstChild).toHaveClass('my-css-class');
   });
 
-  it('should have default medium size', () => {
-    const { container } = render(<Input className="my-css-class" />);
-    expect(container.firstChild).toHaveClass(styles['input--medium']);
-  });
-
-  it('should allow for compact size', () => {
-    const { container } = render(<Input inputSize="compact" />);
-    expect(container.firstChild).toHaveClass(styles['input--compact']);
-  });
-
-  it('should allow for medium size', () => {
-    const { container } = render(<Input inputSize="medium" />);
-    expect(container.firstChild).toHaveClass(styles['input--medium']);
-  });
-
-  it('should allow for large size', () => {
-    const { container } = render(<Input inputSize="large" />);
-    expect(container.firstChild).toHaveClass(styles['input--large']);
-  });
-
-  it('should have error class on error', () => {
-    const { container } = render(<Input error />);
-    expect(container.firstChild).toHaveClass(styles['input--error']);
-  });
-
   it('should have disabled class and input should be disabled if "disabled" prop is set', () => {
-    const { container, getByRole } = render(<Input disabled />);
-    expect(container.firstChild).toHaveClass(styles['input--disabled']);
+    const { container, getByRole } = renderComponent({ disabled: true });
+
     expect(container.firstChild).toHaveAttribute('aria-disabled', 'true');
     expect(getByRole('textbox')).toHaveAttribute('disabled');
   });
 
   it('should have custom placeholder text if it is set', () => {
-    const { getByRole } = render(<Input placeholder="Custom placeholder" />);
+    const { getByRole } = renderComponent({
+      placeholder: 'Custom placeholder',
+    });
+
     expect(getByRole('textbox')).toHaveAttribute(
       'placeholder',
       'Custom placeholder'
@@ -57,68 +36,79 @@ describe('<Input> component', () => {
   });
 
   it('should have text type input as default', () => {
-    const { getByRole } = render(<Input />);
+    const { getByRole } = renderComponent({});
+
     expect(getByRole('textbox')).toHaveAttribute('type', 'text');
   });
 
   it('should have password type input if type "password" is set', () => {
-    const { getByTestId } = render(
-      <Input type="password" data-testid="password-input" />
-    );
+    const { getByTestId } = renderComponent({
+      type: 'password',
+      [`data-testid`]: 'password-input',
+    });
+
     expect(getByTestId('password-input')).toHaveAttribute('type', 'password');
   });
 
   it('should change the input type if show password icon is clicked', () => {
-    const { getByRole, getByTestId } = render(
-      <Input type="password" data-testid="password-input" />
-    );
+    const { getByRole, getByTestId } = renderComponent({
+      type: 'password',
+      [`data-testid`]: 'password-input',
+    });
     const input = getByTestId('password-input');
     const button = getByRole('button');
 
     expect(input).toHaveAttribute('type', 'password');
-    fireEvent.click(button);
+    userEvent.click(button);
     expect(input).toHaveAttribute('type', 'text');
-    fireEvent.click(button);
+    userEvent.click(button);
     expect(input).toHaveAttribute('type', 'password');
   });
 
   it('should render with icon from the left side', () => {
-    const { getByTestId } = render(
-      <Input
-        icon={{
-          source: <Icon source={AddCircleIcon} />,
-          place: 'left',
-        }}
-      />
-    );
+    const { getByTestId } = renderComponent({
+      icon: { source: <Icon source={AddCircleIcon} />, place: 'left' },
+    });
 
-    expect(getByTestId('input-icon-left')).toBeVisible();
+    expect(getByTestId('input-icon-left')).toBeInTheDocument();
   });
 
   it('should render with icon from the right side', () => {
-    const { getByTestId } = render(
-      <Input
-        icon={{
-          source: <Icon source={AddCircleIcon} />,
-          place: 'right',
-        }}
-      />
-    );
+    const { getByTestId } = renderComponent({
+      icon: { source: <Icon source={AddCircleIcon} />, place: 'right' },
+    });
 
     expect(getByTestId('input-icon-right')).toBeVisible();
   });
 
   it('should not render with icon from the right side if type "password" is set', () => {
-    const { queryByTestId } = render(
-      <Input
-        type="password"
-        icon={{
-          source: <Icon source={AddCircleIcon} />,
-          place: 'right',
-        }}
-      />
-    );
+    const { queryByTestId } = renderComponent({
+      type: 'password',
+      icon: { source: <Icon source={AddCircleIcon} />, place: 'right' },
+    });
 
-    expect(queryByTestId('input-icon-right')).toBeFalsy();
+    expect(queryByTestId('input-icon-right')).not.toBeInTheDocument();
+  });
+
+  it('should call onChange if input value change', () => {
+    const onChangeFunction = vi.fn();
+    const InputWrapper = () => {
+      const [value, setValue] = React.useState('');
+
+      return (
+        <Input
+          value={value}
+          onChange={(v) => {
+            setValue(v.currentTarget.value);
+            onChangeFunction(v.currentTarget.value);
+          }}
+        />
+      );
+    };
+    const { getByRole } = render(<InputWrapper />);
+    const text = 'test value';
+
+    userEvent.type(getByRole('textbox'), text, { delay: 0 });
+    expect(onChangeFunction).toHaveBeenCalledWith(text);
   });
 });
