d720aab5ef84fd7db6860832cfae4a84addd56b5
Marcin Sawicki <m.sawicki@livechat.com>
2024-06-18 10:59:17 +0200

    fix(Icon): changes after review

--- packages/react-components/src/components/Icon/Icon.stories.tsx
+++ packages/react-components/src/components/Icon/Icon.stories.tsx
@@ -3,13 +3,15 @@ import * as React from 'react';
 import * as TablerIcons from '@livechat/design-system-icons';
 import { Meta } from '@storybook/react';
 
-import { Icon as IconComponent, IconProps } from './Icon';
+import { StoryDescriptor } from '../../stories/components/StoryDescriptor';
+
+import { Icon, IconProps } from './Icon';
 
 const iterator = Object.keys(TablerIcons);
 
 export default {
   title: 'Components/Icon',
-  component: IconComponent,
+  component: Icon,
   argTypes: {
     source: {
       options: iterator,
@@ -34,17 +36,43 @@ export default {
       },
     },
   },
-} as Meta<typeof IconComponent>;
+} as Meta<typeof Icon>;
 
-export const Icon = (args: IconProps): React.ReactElement => (
+export const Default = (args: IconProps): React.ReactElement => (
   <div style={{ width: '300px' }}>
-    <IconComponent {...args} />
+    <Icon {...args} />
   </div>
 );
 
-Icon.args = {
+Default.args = {
   source: TablerIcons.EmailLetter,
   size: 'medium',
   kind: 'primary',
   disabled: false,
 };
+
+export const Sizes = (): React.ReactElement => (
+  <div>
+    <StoryDescriptor title="xsmall">
+      <Icon source={TablerIcons.EmailLetter} size="xsmall" />
+    </StoryDescriptor>
+    <StoryDescriptor title="small">
+      <Icon source={TablerIcons.EmailLetter} size="small" />
+    </StoryDescriptor>
+    <StoryDescriptor title="medium">
+      <Icon source={TablerIcons.EmailLetter} size="medium" />
+    </StoryDescriptor>
+    <StoryDescriptor title="large">
+      <Icon source={TablerIcons.EmailLetter} size="large" />
+    </StoryDescriptor>
+    <StoryDescriptor title="xlarge">
+      <Icon source={TablerIcons.EmailLetter} size="xlarge" />
+    </StoryDescriptor>
+    <StoryDescriptor title="xxlarge">
+      <Icon source={TablerIcons.EmailLetter} size="xxlarge" />
+    </StoryDescriptor>
+    <StoryDescriptor title="xxxlarge">
+      <Icon source={TablerIcons.EmailLetter} size="xxxlarge" />
+    </StoryDescriptor>
+  </div>
+);
