4f846f7cd0f9e2cef288cc95741181cdbf77001e
Marcin Sawicki <m.sawicki@livechatinc.com>
2024-06-18 10:56:40 +0200

    fix(FieldGroup): tests updates (#1181)

--- packages/react-components/src/components/FieldGroup/FieldGroup.spec.tsx
+++ packages/react-components/src/components/FieldGroup/FieldGroup.spec.tsx
@@ -4,26 +4,12 @@ import { render } from 'test-utils';
 
 import { FieldGroup } from './FieldGroup';
 
-import styles from './FieldGroup.module.scss';
-
-const baseClass = 'field-group';
-
 describe('<FieldGroup> component', () => {
   it('should allow for custom class', () => {
     const { container } = render(
       <FieldGroup className="my-css-class">test</FieldGroup>
     );
     expect(container.firstChild).toHaveClass('my-css-class');
-  });
-
-  it('should have inline class when inline prop is applied', () => {
-    const { container } = render(<FieldGroup inline>test</FieldGroup>);
-    expect(container.firstChild).toHaveClass(styles[`${baseClass}--inline`]);
-  });
-
-  it('should have stretched class when stretched prop is applied', () => {
-    const { container } = render(<FieldGroup stretch>test</FieldGroup>);
-    expect(container.firstChild).toHaveClass(styles[`${baseClass}--stretched`]);
   });
 
   it('should render error text', () => {
