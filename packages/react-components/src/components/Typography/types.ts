bbbe6fa687927769907b41c7d85d250c780abe24
Marcin Sawicki <m.sawicki@livechatinc.com>
2024-06-24 09:06:55 +0200

    fix(Typography): styles update and new paragraph size (#1196)
    Co-authored-by: Vadym <12149393+VadymBezpalko@users.noreply.github.com>

--- packages/react-components/src/components/Typography/types.ts
+++ packages/react-components/src/components/Typography/types.ts
@@ -1,4 +1,4 @@
-export type TTextSize = 'xs' | 'sm' | 'md';
+export type TTextSize = 'xs' | 'sm' | 'md' | 'lg';
 export type THeadingSize = '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 export type TDisplaySize = 'sm' | 'md' | 'lg' | 'max';
 export type TTextAlign = 'left' | 'right' | 'center' | 'justify';
