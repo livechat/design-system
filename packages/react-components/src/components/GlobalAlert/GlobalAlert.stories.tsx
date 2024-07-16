a4c4bf83e7fba7f5d4a4b56c2e96cf5d8c324542
Vadym <v.bezpalko@livechat.com>
2024-07-16 16:07:59 +0200

    feat(GlobalAlert): storybook styling

--- packages/react-components/src/components/GlobalAlert/GlobalAlert.stories.tsx
+++ packages/react-components/src/components/GlobalAlert/GlobalAlert.stories.tsx
@@ -2,11 +2,21 @@ import * as React from 'react';
 
 import { Meta, StoryObj } from '@storybook/react';
 
+import { Button } from '../Button';
+import { Text } from '../Typography';
+
 import { GlobalAlert as GlobalAlertComponent } from './GlobalAlert';
 
 const meta: Meta<typeof GlobalAlertComponent> = {
   title: 'Components/GlobalAlert',
   component: GlobalAlertComponent,
+  decorators: [
+    (Story) => (
+      <div style={{ display: 'flex' }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default meta;
@@ -15,10 +25,32 @@ type Story = StoryObj<typeof GlobalAlertComponent>;
 export const Default: Story = {
   args: {
     kind: 'info',
+    label: 'Info alert',
+    children: (
+      <Text>Some really important information with very long description</Text>
+    ),
   },
-  render: ({ kind }) => (
-    <GlobalAlertComponent kind={kind} label="Info alert">
-      Some more text
-    </GlobalAlertComponent>
-  ),
+};
+
+export const TwoLineLabel: Story = {
+  args: {
+    kind: 'info',
+    label: 'Info alert with really really really really long label',
+    children: (
+      <Text>Some really important information with very long description</Text>
+    ),
+  },
+};
+
+export const LongContent: Story = {
+  args: {
+    kind: 'info',
+    label: 'Long content alert',
+    children: (
+      <>
+        <Text>Some description text here</Text>
+        <Button>Start accepting chats</Button>
+      </>
+    ),
+  },
 };
