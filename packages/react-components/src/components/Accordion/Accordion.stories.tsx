8ab71a8c9869a7f0ac42c1c6655d81aab8a3d835
Marcin Sawicki <m.sawicki@livechat.com>
2024-08-29 15:55:34 +0200

    feat(Accordion): styles and keyboard controls

--- packages/react-components/src/components/Accordion/Accordion.stories.tsx
+++ packages/react-components/src/components/Accordion/Accordion.stories.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 
+import { Tag as TagIcon } from '@livechat/design-system-icons';
 import { Meta } from '@storybook/react';
 
+import { StoryDescriptor } from '../../stories/components/StoryDescriptor';
+import { Icon } from '../Icon';
+import { IPickerListItem, Picker } from '../Picker';
+import { Tag } from '../Tag';
+
 import { Accordion } from './Accordion';
+import { RULE_PICKER_OPTIONS, TAGS_PICKER_OPTIONS } from './stories-helpers';
+
+import './Accordion.stories.css';
 
 export default {
   title: 'Components/Accordion',
@@ -10,9 +19,73 @@ export default {
 } as Meta<typeof Accordion>;
 
 export const Default = (): React.ReactElement => {
+  return <Accordion label="Accordion label">Accordion content</Accordion>;
+};
+
+export const Kinds = (): React.ReactElement => {
   return (
     <div>
-      <Accordion label="Accordion label">Accordion content</Accordion>
+      <StoryDescriptor title="Default">
+        <Accordion label="Default" kind="default">
+          Default accordion content
+        </Accordion>
+      </StoryDescriptor>
+      <StoryDescriptor title="Warning">
+        <Accordion label="Warning" kind="warning">
+          Warning accordion content
+        </Accordion>
+      </StoryDescriptor>
+      <StoryDescriptor title="Error">
+        <Accordion label="Error" kind="error">
+          Error accordion content
+        </Accordion>
+      </StoryDescriptor>
+    </div>
+  );
+};
+
+export const Example = (): React.ReactElement => {
+  const [ruleSelected, setRuleSelected] = React.useState<
+    IPickerListItem[] | null
+  >([RULE_PICKER_OPTIONS[0]]);
+  const [tagSelected, setTagSelected] = React.useState<
+    IPickerListItem[] | null
+  >([TAGS_PICKER_OPTIONS[0]]);
+
+  return (
+    <div>
+      <Accordion
+        label={
+          <div className="label">
+            <Icon source={TagIcon} />
+            Tag <Tag>{ruleSelected && ruleSelected[0].name}</Tag>
+            and
+            {tagSelected &&
+              tagSelected &&
+              tagSelected.map((tag) => <Tag key={tag.key}>{tag.name}</Tag>)}
+          </div>
+        }
+      >
+        <div className="accordion-content-container">
+          <div className="rule-picker">
+            <Picker
+              id="rule-picker"
+              options={RULE_PICKER_OPTIONS}
+              selected={ruleSelected}
+              hideClearButton
+              searchDisabled
+              onSelect={(selected) => setRuleSelected(selected)}
+            />
+          </div>
+          <Picker
+            id="first-picker"
+            type="multi"
+            options={TAGS_PICKER_OPTIONS}
+            selected={tagSelected}
+            onSelect={(selected) => setTagSelected(selected)}
+          />
+        </div>
+      </Accordion>
     </div>
   );
 };
--- packages/react-components/src/components/Accordion/Accordion.tsx
+++ packages/react-components/src/components/Accordion/Accordion.tsx
@@ -28,26 +28,44 @@ export interface IAccordionProps extends ComponentCoreProps {
    * Specify the kind of the accordion
    */
   kind?: 'default' | 'warning' | 'error';
+  /**
+   * Specify if the accordion should be open on init
+   */
+  openOnInit?: boolean;
 }
 
 export const Accordion: React.FC<IAccordionProps> = ({
+  className,
   children,
   label,
   multilineElement,
   kind = 'default',
+  openOnInit = false,
 }) => {
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(openOnInit);
   const contentRef = React.useRef<HTMLDivElement>(null);
   const [size, setSize] = React.useState(0);
   const previousSizeRef = React.useRef(size);
+  const mergedClassName = cx(
+    styles[baseClass],
+    styles[`${baseClass}--${kind}`],
+    {
+      [styles[`${baseClass}--open`]]: isOpen,
+    },
+    className
+  );
 
   const handleClick = () => {
     setIsOpen((prev) => !prev);
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (event.key === 'Enter' || event.key === ' ') {
+    if (!isOpen && (event.key === 'Enter' || event.key === ' ')) {
       setIsOpen((prev) => !prev);
+    }
+
+    if (isOpen && event.key === 'Escape') {
+      setIsOpen(false);
     }
   };
 
@@ -77,10 +95,7 @@ export const Accordion: React.FC<IAccordionProps> = ({
     <div
       tabIndex={0}
       aria-expanded={isOpen}
-      className={cx(styles[baseClass], styles[`${baseClass}--${kind}`], {
-        [styles[`${baseClass}--open`]]: isOpen,
-      })}
-      onClick={handleClick}
+      className={mergedClassName}
       onKeyDown={handleKeyDown}
     >
       <Icon
@@ -89,7 +104,11 @@ export const Accordion: React.FC<IAccordionProps> = ({
           [styles[`${baseClass}__chevron--open`]]: isOpen,
         })}
       />
-      <Text bold className={styles[`${baseClass}__label`]}>
+      <Text
+        bold
+        className={styles[`${baseClass}__label`]}
+        onClick={handleClick}
+      >
         {label}
       </Text>
       {multilineElement && (
@@ -102,7 +121,12 @@ export const Accordion: React.FC<IAccordionProps> = ({
         style={{ maxHeight: isOpen ? size : 0 }}
       >
         <div aria-expanded={isOpen} ref={contentRef}>
-          <Text as="div" className={styles[`${baseClass}__content__inner`]}>
+          <Text
+            as="div"
+            className={cx(styles[`${baseClass}__content__inner`], {
+              [styles[`${baseClass}__content__inner--open`]]: isOpen,
+            })}
+          >
             {children}
           </Text>
         </div>
