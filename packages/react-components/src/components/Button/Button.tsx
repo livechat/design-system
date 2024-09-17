acae5d79f782d3b64dfdb396c0f962a77ad4cdb3
Marcin Sawicki <m.sawicki@livechatinc.com>
2024-09-17 15:03:57 +0200

    feat(Button): animated label (#1351)

--- packages/react-components/src/components/Button/Button.tsx
+++ packages/react-components/src/components/Button/Button.tsx
@@ -4,7 +4,12 @@ import cx from 'clsx';
 
 import { Loader } from '../Loader';
 
-import { getSpinnerColors } from './Button.helpers';
+import {
+  buttonRef,
+  getSpinnerColors,
+  handleKeyboardInteraction,
+  handleMouseInteraction,
+} from './Button.helpers';
 import { ButtonKind, ButtonSize } from './types';
 
 import styles from './Button.module.scss';
@@ -38,6 +43,10 @@ export type ButtonProps = {
    * Specify the place to render element given in `icon` prop
    */
   iconPosition?: 'left' | 'right';
+  /**
+   * Set to enable animation that will show label on button hover if icon is present
+   */
+  animatedLabel?: boolean;
 } & React.ButtonHTMLAttributes<HTMLButtonElement> &
   React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
@@ -62,12 +71,16 @@ export const Button = React.forwardRef<
       children,
       href,
       onClick,
+      animatedLabel,
       ...props
     },
     ref
   ) => {
+    const [labelWidth, setLabelWidth] = React.useState(0);
+    const [isLabelOpen, setIsLabelOpen] = React.useState(false);
     const isDisabled = loading || disabled;
     const isIconOnly = !children && icon;
+    const isAnimatedLabel = animatedLabel && children && icon;
     const isTextButton = ['text', 'link', 'link-light'].includes(kind);
 
     const Component = href ? 'a' : 'button';
@@ -85,6 +98,7 @@ export const Button = React.forwardRef<
         [styles[`${baseClass}--icon-only`]]: isIconOnly,
         [styles[`${baseClass}--icon-only--bg`]]: isIconOnly && isTextButton,
         [styles[`${baseClass}--disabled`]]: isDisabled,
+        [styles[`${baseClass}--animated-label`]]: isAnimatedLabel,
       }
     );
 
@@ -97,6 +111,32 @@ export const Button = React.forwardRef<
         disabled={isDisabled}
         {...(!isDisabled && { href, onClick })}
         {...props}
+        {...(isAnimatedLabel && {
+          onMouseEnter: (e) =>
+            handleMouseInteraction(
+              e,
+              () => setIsLabelOpen(true),
+              props?.onMouseEnter
+            ),
+          onMouseLeave: (e) =>
+            handleMouseInteraction(
+              e,
+              () => setIsLabelOpen(false),
+              props?.onMouseLeave
+            ),
+          onFocus: (e) =>
+            handleKeyboardInteraction(
+              e,
+              () => setIsLabelOpen(true),
+              props?.onFocus
+            ),
+          onBlur: (e) =>
+            handleKeyboardInteraction(
+              e,
+              () => setIsLabelOpen(false),
+              props?.onBlur
+            ),
+        })}
       >
         {loading && (
           <Loader
@@ -120,7 +160,22 @@ export const Button = React.forwardRef<
             disabled,
             size: size === 'xcompact' ? 'small' : 'medium',
           })}
-        <div className={styles[`${baseClass}__content`]}>{children}</div>
+        <div
+          className={styles[`${baseClass}__content`]}
+          style={
+            isAnimatedLabel
+              ? { maxWidth: isLabelOpen ? labelWidth : 0 }
+              : undefined
+          }
+        >
+          {isAnimatedLabel ? (
+            <span ref={(node) => buttonRef(node, setLabelWidth)}>
+              {children}
+            </span>
+          ) : (
+            children
+          )}
+        </div>
       </Component>
     );
   }
