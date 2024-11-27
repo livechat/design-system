00491ab7d92fabc20ef887757b3b6aa04b7ee216
Artur Fracala <arturfracala@MacBook-Pro-Artur.local>
2024-11-27 04:00:29 -0500

    Feat(Table): add storybook

---
+++ packages/react-components/src/components/Table/Table.stories.tsx
@@ -0,0 +1,304 @@
+import * as React from 'react';
+
+import { Meta, StoryFn } from '@storybook/react';
+
+import { StoryDescriptor } from '../../stories/components/StoryDescriptor';
+import { Button } from '../Button';
+
+import { Table } from './Table';
+import { Column } from './types';
+
+export default {
+  title: 'Components/Table',
+  component: Table,
+} as Meta<typeof Table>;
+
+type Data = {
+  id: number;
+  name: string;
+  age: number;
+  role: string;
+  action: React.ReactNode;
+};
+
+const columns: Column<(typeof data)[0]>[] = [
+  {
+    key: 'id',
+    header: 'ID',
+    sortable: true,
+  },
+  {
+    key: 'name',
+    header: 'Name',
+    sortable: true,
+    sortValue: (item: Data) => item.name.toLowerCase(),
+  },
+  {
+    key: 'age',
+    header: 'Age',
+    sortable: true,
+  },
+  {
+    key: 'role',
+    header: 'Role',
+    sortable: true,
+    sortValue: (item: Data) => item.role,
+  },
+  {
+    key: 'action',
+    header: 'Action',
+    sortable: false,
+    sortValue: undefined,
+  },
+];
+const data: Data[] = [
+  {
+    id: 1,
+    name: 'John Doe',
+    age: 28,
+    role: 'Developer',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    age: 34,
+    role: 'Designer',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 3,
+    name: 'Alice Johnson',
+    age: 42,
+    role: 'Manager',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 4,
+    name: 'Mike Williams',
+    age: 25,
+    role: 'Intern',
+    action: <Button>Edit</Button>,
+  },
+];
+
+export const Default: StoryFn = () => {
+  const getRowId = (row: Data) => row.id;
+
+  return (
+    <Table size="medium" data={data} columns={columns} getRowId={getRowId} />
+  );
+};
+
+export const Resizing: StoryFn = () => {
+  const getRowId = (row: Data) => row.id;
+
+  return (
+    <Table
+      size="medium"
+      data={data}
+      columns={columns}
+      getRowId={getRowId}
+      resizable
+    />
+  );
+};
+
+export const Sizes: StoryFn = (): React.ReactElement => {
+  const getRowId = (row: Data) => row.id;
+
+  return (
+    <>
+      <StoryDescriptor title="Large">
+        <Table size="large" data={data} columns={columns} getRowId={getRowId} />
+      </StoryDescriptor>
+      <StoryDescriptor title="Medium">
+        <Table
+          size="medium"
+          data={data}
+          columns={columns}
+          getRowId={getRowId}
+        />
+      </StoryDescriptor>
+      <StoryDescriptor title="Small">
+        <Table size="small" data={data} columns={columns} getRowId={getRowId} />
+      </StoryDescriptor>
+    </>
+  );
+};
+
+const dataForPinnExample: Data[] = [
+  {
+    id: 1,
+    name: 'John Doe',
+    age: 28,
+    role: 'Developer',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    age: 34,
+    role: 'Designer',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 3,
+    name: 'Alice Johnson',
+    age: 42,
+    role: 'Manager',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 4,
+    name: 'Mike Williams',
+    age: 25,
+    role: 'Intern',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 5,
+    name: 'David Brown',
+    age: 30,
+    role: 'Developer',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 6,
+    name: 'Laura Wilson',
+    age: 27,
+    role: 'Designer',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 7,
+    name: 'Sam Green',
+    age: 45,
+    role: 'Manager',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 8,
+    name: 'Emily Clark',
+    age: 22,
+    role: 'Intern',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 9,
+    name: 'Chris Adams',
+    age: 36,
+    role: 'Developer',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 10,
+    name: 'Sophia Turner',
+    age: 29,
+    role: 'Designer',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 11,
+    name: 'James Taylor',
+    age: 39,
+    role: 'Manager',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 12,
+    name: 'Olivia Lewis',
+    age: 24,
+    role: 'Intern',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 13,
+    name: 'Daniel Evans',
+    age: 32,
+    role: 'Developer',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 14,
+    name: 'Mia Harris',
+    age: 31,
+    role: 'Designer',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 15,
+    name: 'Noah King',
+    age: 40,
+    role: 'Manager',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 16,
+    name: 'Liam Scott',
+    age: 23,
+    role: 'Intern',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 17,
+    name: 'Ethan White',
+    age: 26,
+    role: 'Developer',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 18,
+    name: 'Emma Hall',
+    age: 35,
+    role: 'Designer',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 19,
+    name: 'Benjamin Young',
+    age: 41,
+    role: 'Manager',
+    action: <Button>Edit</Button>,
+  },
+  {
+    id: 20,
+    name: 'Ava Martinez',
+    age: 21,
+    role: 'Intern',
+    action: <Button>Edit</Button>,
+  },
+];
+
+export const Pinning: StoryFn = () => {
+  const getRowId = (row: Data) => row.id;
+
+  return (
+    <Table
+      size="medium"
+      data={dataForPinnExample}
+      columns={columns}
+      getRowId={getRowId}
+      pin="header"
+    />
+  );
+};
+
+export const Selecting: StoryFn = () => {
+  const getRowId = (row: Data) => row.id;
+  const [selectedRows, setSelectedRows] = React.useState<Set<string | number>>(
+    new Set()
+  );
+
+  return (
+    <Table
+      size="medium"
+      data={data}
+      columns={columns}
+      getRowId={getRowId}
+      selectable
+      selectedRows={selectedRows}
+      onSelectionChange={setSelectedRows}
+    />
+  );
+};
--- packages/react-components/src/components/Table/types.ts
+++ packages/react-components/src/components/Table/types.ts
@@ -16,15 +16,50 @@ export enum SortOrder {
 }
 
 export interface ITableProps<T> {
+  /**
+   * The data to be displayed in the table. Each item represents a row.
+   */
   data: T[];
+  /**
+   * The configuration for table columns, including keys, headers, and optional sorting logic.
+   */
   columns: Column<T>[];
+  /**
+   * Whether to display alternating row background colors for better readability.
+   */
   stripped?: boolean;
+  /**
+   * Specifies the size of the table, which affects row height and spacing.
+   * Options: 'small', 'medium', 'large'.
+   */
   size?: TableSize;
+  /**
+   * Determines if certain table features, like the header, should remain pinned.
+   * Currently supports only `header` pinning.
+   */
   pin?: PinOptions;
+  /**
+   * A function that returns a unique identifier for each row, used for selection and key assignment.
+   * @param item - The data item representing a row.
+   * @returns A unique identifier (string or number) for the row.
+   */
   getRowId: (item: T) => string | number;
+  /**
+   * Enables or disables the ability to resize columns by dragging the edges of headers.
+   */
   resizable?: boolean;
+  /**
+   * Enables row selection mode, adding checkboxes to each row and the header for selection.
+   */
   selectable?: true;
+  /**
+   * A set of currently selected row IDs. Useful for controlling the selection state externally.
+   */
   selectedRows?: Set<string | number>;
+  /**
+   * A callback function triggered when the selected rows change.
+   * @param selectedIds - A set of selected row IDs.
+   */
   onSelectionChange?: (selectedIds: Set<string | number>) => void;
 }
 
