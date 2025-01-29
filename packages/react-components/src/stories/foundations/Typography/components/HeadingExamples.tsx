c84ad3fc24a17e87e0bb4d9da062f666db3793e0
joannasikora <jo.sikora21@gmail.com>
2025-01-29 11:37:11 +0100

    feat(Typography): swap React.Fragment and fragment syntax in HeadingExamples

--- packages/react-components/src/stories/foundations/Typography/components/HeadingExamples.tsx
+++ packages/react-components/src/stories/foundations/Typography/components/HeadingExamples.tsx
@@ -5,19 +5,19 @@ import { Heading, HEADING_SIZES } from '../../../../components/Typography';
 export const HeadingExamples: React.FC = () => {
   const elements = HEADING_SIZES.map((size) => {
     return (
-      <>
+      <React.Fragment key={size}>
         <Heading size={size}>Heading {size.toUpperCase()}</Heading>
         {['2xs', '3xs'].includes(size) && (
-          <React.Fragment key={size}>
+          <>
             <Heading size={size} uppercase>
               Heading {size.toUpperCase()} with uppercase
             </Heading>
             <Heading size={size} uppercase bold>
               Heading {size.toUpperCase()} with uppercase and bold
             </Heading>
-          </React.Fragment>
+          </>
         )}
-      </>
+      </React.Fragment>
     );
   });
 
